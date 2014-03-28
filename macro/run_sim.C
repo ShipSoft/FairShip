2a112dfbd1a8377b239def197692b289e9ebd19e
Mohammad Al-Turany <m.al-turany@gsi.de>
2014-03-28 21:37:24 +0100

    Add field directory with some basic implimentation and correct bug  for Boost serialization

--- macro/run_sim.C
+++ macro/run_sim.C
@@ -57,6 +57,18 @@ void run_sim(Int_t nEvents = 10, TString mcEngine = "TGeant3")
 
  // ------------------------------------------------------------------------
 
+
+    // -----   Magnetic field   -------------------------------------------
+    // Constant Field
+    ShipConstField  *fMagField = new ShipConstField();
+    fMagField->SetField(0., 2. ,0. ); // values are in kG
+    fMagField->SetFieldRegion(-160, 160,-160, 160, 1940, 125); // values are in cm
+                          //  (xmin,xmax,ymin,ymax,zmin,zmax)
+    run->SetField(fMagField);
+    // --------------------------------------------------------------------
+
+    
+    
   // -----   Create PrimaryGenerator   --------------------------------------
   FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
   
