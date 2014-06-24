4034d5efd9137edd555bcf2abe6a48ee082f0e72
Thomas Ruf <thomas.ruf@cern.ch>
2014-06-24 13:32:29 +0200

    towards realistic lifetime

--- shipdata/ShipStack.cxx
+++ shipdata/ShipStack.cxx
@@ -93,6 +93,7 @@ void ShipStack::PushTrack(Int_t toBeDone, Int_t parentId, Int_t pdgCode,
                           Double_t polz, TMCProcess proc, Int_t& ntr,
                           Double_t weight, Int_t is, Int_t secondparentID)
 {
+  //cout << "ShipStack:  " << pz << " " << parentId << " " << secondparentID << endl;
 
   // --> Get TParticle array
   TClonesArray& partArray = *fParticles;
