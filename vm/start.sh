6703aa9bc69d59ceb7e8e70410ab8ad427085180
Andrey Ustyuzhanin <andrey.u@gmail.com>
2014-07-14 14:56:05 +0300

    VM diagnostics

--- vm/start.sh
+++ vm/start.sh
@@ -1,3 +1,22 @@
-export VAGRANT_CWD=`pwd`
-export VAGRANT_DOTFILE_PATH=`pwd`
-vagrant up --provider=docker
+#!/bin/bash
+export BASEDIR=$(echo $(cd $(dirname "$0")/.. && pwd -P))
+
+if [ "$1" = "diag" ] ; then
+	echo "=== DIAGNOSTIC ==="
+	cat /etc/resolv.conf
+	netstat -nr
+	ifconfig -a
+	nslookup cdn-registry-1.docker.io
+	# traceroute -q 2 -m 25 cdn-registry-1.docker.io
+	ping -c 3 cdn-registry-1.docker.io
+	curl https://cdn-registry-1.docker.io/
+	echo
+	echo "=== END OF DIAGNOSTIC ==="
+	cd $BASEDIR/diag
+	vagrant up --provider=docker
+else
+	export VAGRANT_CWD=$BASEDIR
+	export VAGRANT_DOTFILE_PATH=$BASEDIR
+	vagrant up --provider=docker
+fi
+
