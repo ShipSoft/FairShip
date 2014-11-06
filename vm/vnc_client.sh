36c58ca213e7c653bb4a024a7a6528b4bb582eaf
Andrey Ustyuzhanin <andrey.u@gmail.com>
2014-11-06 10:15:16 +0300

    VM update @Mac

--- vm/vnc_client.sh
+++ vm/vnc_client.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+VERBOSE=0
 [[ $OSTYPE != darwin* ]] && echo "this script is for Mac only" && exit 1
+[ "$1" = "-n" ] && DRYRUN=1
 
 docker_ip() {
   boot2docker ip 2>/dev/null
@@ -8,5 +10,10 @@ docker_ip() {
 
 ip=`docker_ip`
 [ ! -n "$ip" ] && echo "unable to determine VM IP" && exit
+DEST=`docker_ip`:5900
 
-open -a "VNC viewer" --args `docker_ip`:5900
+if [ $DRYRUN -eq 1 ] ; then
+  echo "Connecting to: $DEST"
+else
+  open -a "VNC viewer" --args $DEST
+fi
