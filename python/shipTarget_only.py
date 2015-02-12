377396e63853911040d87c12ad190c86a32251a6
Thomas Ruf <thomas.ruf@cern.ch>
2015-02-12 17:41:40 +0100

    start cleaning up a bit

---
+++ python/shipTarget_only.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+import ROOT,os
+import shipunit as u
+
+def configure(run,ship_geo):
+# -----Create media-------------------------------------------------
+ run.SetMaterials("media.geo")  # Materials
+# ------------------------------------------------------------------------
+  
+# -----Create geometry----------------------------------------------
+ cave= ROOT.ShipCave("CAVE")
+ cave.SetGeometryFileName("cave.geo")
+ run.AddModule(cave)
+
+ TargetStation = ROOT.ShipTargetStation("TargetStation",ship_geo.target.length,ship_geo.hadronAbsorber.length,
+                                                        ship_geo.target.z,ship_geo.hadronAbsorber.z,ship_geo.targetOpt,ship_geo.target.sl)
+ if ship_geo.targetOpt>10:
+  TargetStation.SetLayerPosMat(ship_geo.target.xy,ship_geo.target.L1,ship_geo.target.M1,ship_geo.target.L2,ship_geo.target.M2,\
+  ship_geo.target.L3,ship_geo.target.M3,ship_geo.target.L4,ship_geo.target.M4,ship_geo.target.L5,ship_geo.target.M5,\
+  ship_geo.target.L6,ship_geo.target.M6,ship_geo.target.L7,ship_geo.target.M7,ship_geo.target.L8,ship_geo.target.M8,\
+  ship_geo.target.L9,ship_geo.target.M9,ship_geo.target.L10,ship_geo.target.M10,ship_geo.target.L11,ship_geo.target.M11,\
+  ship_geo.target.L12,ship_geo.target.M12,ship_geo.target.L13,ship_geo.target.M13,ship_geo.target.L14,ship_geo.target.M14,\
+  ship_geo.target.L15,ship_geo.target.M15,ship_geo.target.L16,ship_geo.target.M16,ship_geo.target.L17,ship_geo.target.M17)
+ run.AddModule(TargetStation)
+
+
+# return list of detector elements
+ detElements = {}
+ for x in run.GetListOfModules(): detElements[x.GetName()]=x
+ return detElements
