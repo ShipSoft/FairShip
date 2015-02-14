566b6e021fb9d7316b63e66f7e9601df325afbcc
root <root@mc02.h.cern.yandex.net>
2015-02-14 16:03:02 +0400

    vm scripts refactoring

--- vm/destroy.sh
+++ vm/destroy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+VM_DIR=`dirname "$0"`
+source $VM_DIR/_functions.sh
+[ $SYSTEM == "Linux" ] && halt "No need to destroy on Linux"
+
 VM_TITLE="FairShip_"
 name=`VBoxManage list vms | grep $VM_TITLE | awk '{print $1}' | sed 's/"//g'| head -1`
 [ -z "$name" ] && echo "Cannot find FairShip proxy VM ($VM_TITLE)" && exit
--- vm/halt.sh
+++ vm/halt.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+VM_DIR=`dirname "$0"`
+source $VM_DIR/_functions.sh
+[ $SYSTEM == "Linux" ] && halt "No need to halt on Linux"
 mode=poweroff
 [ -n "$1" ] && mode=$1
 uuid=`VBoxManage list runningvms | grep 'FairShip' | awk '{print $2}'| sed s/[{}]//g`
