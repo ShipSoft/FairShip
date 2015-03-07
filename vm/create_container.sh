875b166a4e14715c613c7690572c4af83f01f6aa
Andrey Ustyuzhanin <andrey.u@gmail.com>
2015-03-07 23:57:10 +0300

    check docker is running before image creation

--- vm/create_container.sh
+++ vm/create_container.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+SCRIPT_NAME=$0
+[ -z "SCRIPT_NAME" ] && SCRIPT_NAME=$BASH_SOURCE
+BASEDIR=$(echo $(cd $(dirname "$SCRIPT_NAME")/.. && pwd -P))
+source $BASEDIR/vm/_functions.sh
+
 DOCKER_USER=$USER
 TAG="0.0.9"
 BLD_DIR="build"
@@ -9,6 +14,7 @@ VMPATH=/opt/ship/FairShip/build
 [ -d "$1" ] && BLD_DIR=$1 && shift
 
 REPO="$DOCKER_USER/ship-dev"
+check_docker_connect
 
 docker images | grep $REPO | grep $TAG -q
 if [ $? -eq 0 ] ; then
@@ -24,9 +30,9 @@ for d in gconfig geometry python macro muonShieldOptimization ; do
   RESTORE_LINKS+=" $d"
 done
 
-path=$(readlink -f $BLD_DIR)
+FULL_BLD_DIR=$(echo $(cd $BLD_DIR && pwd -P))
 
-cat > $path/Dockerfile << EOF
+cat > $FULL_BLD_DIR/Dockerfile << EOF
 FROM busybox
 MAINTAINER Andrey Ustyuzhanin andrey.ustyuzhanin@cern.ch
 WORKDIR $VMPATH
@@ -34,7 +40,7 @@ WORKDIR $VMPATH
 ADD . $VMPATH
 
 EOF
-docker build --rm -t $REPO:$TAG $path
+docker build --rm -t $REPO:$TAG $FULL_BLD_DIR
 pushd $BLD_DIR
 for d in $RESTORE_LINKS ; do rm -rf $d ; ln -s ../$d . ;  done
 popd
