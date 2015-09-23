41bb39458d849d3e5621256138e6b63fecccac8e
Andrey Ustyuzhanin <andrey.u@gmail.com>
2015-09-23 13:42:51 +0300

    bash fix

--- vm/_common.sh
+++ vm/_common.sh
@@ -1,5 +1,5 @@
 SCRIPT_NAME=$0
-[ -z "SCRIPT_NAME" ] && SCRIPT_NAME=$BASH_SOURCE
+[[ -z "SCRIPT_NAME" || "$SCRIPT_NAME" == "bash" ]] && SCRIPT_NAME=$BASH_SOURCE
 VM_DIR=`dirname "$SCRIPT_NAME"`
 source $VM_DIR/_functions.sh
 
