e156c90443a2fbdb75c1cb132a51a2d0471b6a92
Thomas Ruf <Thomas.Ruf@cern.ch>
2016-06-30 15:40:54 +0200

    both charm hadrons will now appear in the same FairShip event

--- shipgen/Pythia8Generator.cxx
+++ shipgen/Pythia8Generator.cxx
@@ -95,86 +95,96 @@ Bool_t Pythia8Generator::ReadEvent(FairPrimaryGenerator* cpg)
   Double_t x,y,z,px,py,pz,dl,e,tof;
   Int_t im,id,key;
   fnRetries =0;
-// take charm or beauty hadron from external file
-// correct for too much Ds produced by pythia6
-   bool l = true; 
-   while(l){ 
+// take charm hadrons from external file
+// correct eventually for too much primary Ds produced by pythia6
+  key=0; 
+  bool l = true; 
+  while(l){ 
      if (fn==fNevents) {fLogger->Warning(MESSAGE_ORIGIN, "End of input file. Rewind.");}
+     fTree->GetEntry((fn+1)%fNevents);
+// check that this and next entry is charm, otherwise continue reading
+     l = false;
+     if (int(mE[0])== 0){ l = true; } 
+     bool isDs = false;
+     if ( int(fabs(hid[0]) ) == 431){ isDs = true; }   
      fTree->GetEntry(fn%fNevents);
-     fn++;
-// check that entry is charm, otherwise continue reading
      if (int(mE[0])== 0){ l = true; }   
-     else if ( int(fabs(hid[0]) ) != 431){ l = false; }
-     else {
+     fn++;
+     if ( int(fabs(hid[0]) ) == 431 || isDs ){
        Double_t rnr = gRandom->Uniform(0,1);
-       if( rnr<fFDs ) { l = false; };
+       if( rnr>fFDs ) { 
+        l = true;
+        fn++;
+       }
      }
-   }          
-   fPythia->event.reset();
-   id = (Int_t)hid[0];
-   fPythia->event.append( id, 1, 0, 0, hpx[0],  hpy[0],  hpz[0],  hE[0],  hM[0], 0., 9. );
+  }
+  for(Int_t c=0; c<2; c++){
+    if(c>0){
+      fTree->GetEntry(fn%fNevents);
+      fn++;
+    }
+    fPythia->event.reset();
+    id = (Int_t)hid[0];
+    fPythia->event.append( id, 1, 0, 0, hpx[0],  hpy[0],  hpz[0],  hE[0],  hM[0], 0., 9. );
 //simulate displaced vertex, Pythia8 will not do it
-   Double_t tau0 = fPythia->particleData.tau0(id); // ctau in mm
-   dl = gRandom->Exp(tau0) / hM[0]; // mm/GeV    
-   
-  fPythia->next();
+    Double_t tau0 = fPythia->particleData.tau0(id); // ctau in mm
+    dl = gRandom->Exp(tau0) / hM[0]; // mm/GeV       
+    fPythia->next();
+    Int_t addedParticles = 0;
+    if(c==0){
+// original particle responsible for interaction, "mother of charm" from external file
+     px=mpx[0];
+     py=mpy[0];
+     pz=mpz[0];
+     x=0.;
+     y=0.;
+     z=0.;
+     id=mid[0];
+     cpg->AddTrack(id,px,py,pz,x/cm,y/cm,z/cm,-1,false);
+     addedParticles +=1;
+    }
+    for(Int_t ii=1; ii<fPythia->event.size(); ii++){
+     id = fPythia->event[ii].id(); 
+     Bool_t wanttracking=false;
+     if(fPythia->event[ii].isFinal()){ wanttracking=true; }
+     if (ii>1){
+      z  = fPythia->event[ii].zProd()+dl*fPythia->event[1].pz();
+      x  = fPythia->event[ii].xProd()+dl*fPythia->event[1].px();
+      y  = fPythia->event[ii].yProd()+dl*fPythia->event[1].py();
+      tof = fPythia->event[ii].tProd()+dl*fPythia->event[1].e()/cm/c_light;
+     }else{
+      z  = fPythia->event[ii].zProd();
+      x  = fPythia->event[ii].xProd();
+      y  = fPythia->event[ii].yProd();
+      tof = fPythia->event[ii].tProd();
+     }
+     pz = fPythia->event[ii].pz();
+     px = fPythia->event[ii].px();  
+     py = fPythia->event[ii].py();  
+     e  = fPythia->event[ii].e();
+     im = fPythia->event[ii].mother1()+key;
+     if (ii==1){im = 0;}
+     cpg->AddTrack(id,px,py,pz,x/cm,y/cm,z/cm,im,wanttracking,e,tof,1.);
+     addedParticles+=1;
+    } 
+    key+=addedParticles;
+  } 
   counter+=1; 
-// copy complete pythia6 and pythia8 event  
-  px=mpx[0];
-  py=mpy[0];
-  pz=mpz[0];
-  x=0.;
-  y=0.;
-  z=0.;
-  id=mid[0];
-  key=0;
+// now the underyling event
   bool lx = true;
-  bool first = true;
   while(lx){      
     fTree->GetEntry(fn%fNevents);
+    lx = false;
     if (mE[0] == 0){
+     lx = true;
      fn++;
-     if (first){
-      first = false; 
-      x=mpx[0]; 
-      y=mpy[0]; 
-      z=mpz[0]; 
-     }
      cpg->AddTrack((Int_t)hid[0],hpx[0],hpy[0],hpz[0],(mpx[0]+fPythia->event[0].xProd())/cm,
                                                       (mpy[0]+fPythia->event[0].yProd())/cm,
                                                       (mpz[0]+fPythia->event[0].zProd())/cm,-1,true);
      // mpx,mpy,mpz are the vertex coordinates with respect to charm hadron, first particle in Pythia is (system) 
-     key++;
-    } else {
-     lx = false;
-    }
+   }
   } 
-  cpg->AddTrack(id,px,py,pz,x/cm,y/cm,z/cm,-1,false);
-
-  for(Int_t ii=1; ii<fPythia->event.size(); ii++){
-    id = fPythia->event[ii].id(); 
-    Bool_t wanttracking=false;
-    if(fPythia->event[ii].isFinal()){ wanttracking=true; }
-    if (ii>1){
-     z  = fPythia->event[ii].zProd()+dl*fPythia->event[1].pz();
-     x  = fPythia->event[ii].xProd()+dl*fPythia->event[1].px();
-     y  = fPythia->event[ii].yProd()+dl*fPythia->event[1].py();
-     tof = fPythia->event[ii].tProd()+dl*fPythia->event[1].e()/cm/c_light;
-    }else{
-     z  = fPythia->event[ii].zProd();
-     x  = fPythia->event[ii].xProd();
-     y  = fPythia->event[ii].yProd();
-     tof = fPythia->event[ii].tProd();
-    }
-    pz = fPythia->event[ii].pz();
-    px = fPythia->event[ii].px();  
-    py = fPythia->event[ii].py();  
-    e  = fPythia->event[ii].e();
-    im = fPythia->event[ii].mother1()+key;
-    if (ii==0){im = key;}
-    cpg->AddTrack(id,px,py,pz,x/cm,y/cm,z/cm,im,wanttracking,e,tof,1.);
-  } 
-
+   
   return kTRUE;
 }
 // -------------------------------------------------------------------------
