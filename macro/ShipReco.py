efdb98f39ce885e685fa3b0555c12e0f42830d84
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-03-29 16:11:45 +0200

    Fix argument parsing where strings were used instead of tuples

--- macro/ShipReco.py
+++ macro/ShipReco.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-inputFile = 'ship.Pythia8-TGeant4.root'
+inputFile = 'ship.conical.Pythia8-TGeant4.root'
 geoFile   = None
 debug = False
 EcalDebugDraw = False
@@ -39,30 +39,30 @@ try:
            ["ecalDebugDraw","inputFile=","geoFile=","nEvents=","noStrawSmearing","noVertexing","saveDisk","realPR","withT0"])
 except getopt.GetoptError:
         # print help information and exit:
-        print ' enter --inputFile=  --geoFile= --nEvents=  --firstEvent=,' 
-        print ' noStrawSmearing: no smearing of distance to wire, default on' 
-        print ' outputfile will have same name with _rec added'   
+        print ' enter --inputFile=  --geoFile= --nEvents=  --firstEvent=,'
+        print ' noStrawSmearing: no smearing of distance to wire, default on'
+        print ' outputfile will have same name with _rec added'  
         sys.exit()
 for o, a in opts:
-        if o in ("noVertexing"):
+        if o in ("noVertexing",):
             vertexing = False
-        if o in ("noStrawSmearing"):
+        if o in ("noStrawSmearing",):
             withNoStrawSmearing = True
-        if o in ("--withT0"):
+        if o in ("--withT0",):
             withT0 = True
-        if o in ("-f", "--inputFile"):
+        if o in ("-f", "--inputFile",):
             inputFile = a
-        if o in ("-g", "--geoFile"):
+        if o in ("-g", "--geoFile",):
             geoFile = a
-        if o in ("-n", "--nEvents="):
+        if o in ("-n", "--nEvents=",):
             nEvents = int(a)
-        if o in ("-Y"): 
+        if o in ("-Y",):
             dy = float(a)
-        if o in ("--ecalDebugDraw"):
+        if o in ("--ecalDebugDraw",):
             EcalDebugDraw = True
-        if o in ("--saveDisk"):
+        if o in ("--saveDisk",):
             saveDisk = True
-	if o in ("--realPR"):
+	if o in ("--realPR",):
             realPR = "_PR"
 if EcalDebugDraw: ROOT.gSystem.Load("libASImage")
 
