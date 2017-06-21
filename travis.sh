4d1bca8681c0b8bae34bb14373478137463a7a4f
Fons Rademakers <Fons.Rademakers@cern.ch>
2017-06-21 11:14:49 +0200

    Need numpy and scipy.

--- travis.sh
+++ travis.sh
@@ -31,21 +31,21 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
    #wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
    #sudo apt-add-repository -y "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main"
    #sudo apt-get update
-   #sudo apt-get -y install python-numpy
-   #sudo apt-get -y install python-scipy
+   sudo apt-get -y install python-numpy
+   sudo apt-get -y install python-scipy
    #sudo apt-get -y install gfortran
    #sudo apt-get -y install gcc-5 g++-5
    #sudo apt-get -y install valgrind
    #sudo apt-get -y install doxygen
-   #sudo apt-get -y install cloc
+   sudo apt-get -y install cloc
    #sudo apt-get -y install clang-format-3.9 clang-tidy-3.9
    #export CC=gcc-5
    #export CXX=g++-5
-  export PYTHON=python
-  export FAIRSOFTTAR="https://cernbox.cern.ch/index.php/s/RUwyXapOiiG3tPZ/download"
-  export FAIRROOTTAR="https://cernbox.cern.ch/index.php/s/JQpquOipHWLaaJB/download"
-  export FAIRSOFT_VERSION=trusty-Jun16
-  export FAIRROOT_VERSION=trusty-Jun16
+   export PYTHON=python
+   export FAIRSOFTTAR="https://cernbox.cern.ch/index.php/s/RUwyXapOiiG3tPZ/download"
+   export FAIRROOTTAR="https://cernbox.cern.ch/index.php/s/JQpquOipHWLaaJB/download"
+   export FAIRSOFT_VERSION=trusty-Jun16
+   export FAIRROOT_VERSION=trusty-Jun16
 fi
 
 # install FairSoft
@@ -64,9 +64,9 @@ ${CXX} -v
 cd $fairship
 
 # run following commands only on Linux
-#if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-  #cloc .
-#fi
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+   cloc .
+fi
 
 # add master branch
 # https://github.com/travis-ci/travis-ci/issues/6069
@@ -82,6 +82,6 @@ $PYTHON ../FairShip/macro/run_simScript.py --test
 
 # run following commands only on Linux
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-  cd ../FairShip
-  #make check-submission
+   cd ../FairShip
+   #make check-submission
 fi
