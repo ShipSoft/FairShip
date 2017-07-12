9539c564771fb6eb9a59bccbcc54e6b97d746da9
Thomas Ruf <Thomas.Ruf@cern.ch>
2017-07-12 17:09:29 +0200

    shipdata/ShipMCTrack.h
    extend run_fixedTarget to include charm and beauty decays

--- muonShieldOptimization/run_fixedTarget.py
+++ muonShieldOptimization/run_fixedTarget.py
@@ -2,15 +2,22 @@
 import ROOT,os,sys,getopt,time,shipRoot_conf
 import shipunit as u
 from ShipGeoConfig import ConfigRegistry
+time.sleep(20)
 
 mcEngine     = "TGeant4"
 simEngine    = "Pythia8"
-runnr     = 1
-nev       = 1000
+runnr        = 1
+nev          = 1000
 checkOverlap = True
-G4only = False
-boostDiMuon = 1.
-boostFactor = 1.
+G4only       = False
+storeOnlyMuons = False
+withEvtGen     = True 
+boostDiMuon    = 1.
+boostFactor    = 1.
+charm          = False
+beauty         = False
+charmInputFile = "root://eoslhcb.cern.ch//eos/ship/data/Charm/Cascade-parp16-MSTP82-1-MSEL4-76Mpot_1.root"
+nStart = 0
 
 outputDir    = "."
 theSeed      = int(10000 * time.time() % 10000000)
@@ -32,30 +39,38 @@ logger = logging.getLogger(os.path.splitext(os.path.basename(os.sys.argv[0]))[0]
 logger.setLevel(logging.INFO)
 
 
-def get_work_dir(run_number):
+def get_work_dir(run_number,tag=None):
   import socket
   host = socket.gethostname()
   job_base_name = os.path.splitext(os.path.basename(os.sys.argv[0]))[0]
-  out_dir = "{host}_{base}_{runnr}".format(host=host, base=job_base_name, runnr=run_number)
+  if tag:  out_dir = "{host}_{base}_{runnr}_{comment}".format(host=host, base=job_base_name, runnr=run_number, comment=tag)
+  else:    out_dir = "{host}_{base}_{runnr}".format(host=host, base=job_base_name, runnr=run_number)
   return out_dir
 
 
 def init():
-  global runnr, nev, ecut, G4onyl, tauOnly,JpsiMainly, work_dir
+  global runnr, nev, ecut, G4only, tauOnly,JpsiMainly, work_dir,Debug,withEvtGen,boostDiMuon,boostFactor,charm,beauty,charmInputFile,nStart,storeOnlyMuons
   logger.info("SHiP proton-on-taget simulator (C) Thomas Ruf, 2017")
 
   ap = argparse.ArgumentParser(
       description='Run SHiP "pot" simulation')
-  ap.add_argument('-d', '--debug', action='store_true')
+  ap.add_argument('-d', '--debug', action='store_true', dest='debug')
   ap.add_argument('-f', '--force', action='store_true', help="force overwriting output directory")
   ap.add_argument('-r', '--run-number', type=int, dest='runnr', default=runnr)
   ap.add_argument('-e', '--ecut', type=float, help="energy cut", dest='ecut', default=ecut)
   ap.add_argument('-n', '--num-events', type=int, help="number of events to generate", dest='nev', default=nev)
   ap.add_argument('-G', '--G4only', action='store_true', dest='G4only',     default=False, help="use Geant4 directly, no Pythia8")
+  ap.add_argument('-P', '--PythiaDecay', action='store_true', dest='pythiaDecay', default=False,  help="use Pythia8 for decays")
+  ap.add_argument('-V', '--EvtGen',      action='store_true', dest='withEvtGen',  default=True,   help="use EvtGen for decays")
   ap.add_argument('-t', '--tau-only',     action='store_true', dest='tauOnly',     default=False)
   ap.add_argument('-J', '--Jpsi-mainly',  action='store_true', dest='JpsiMainly',  default=False)
-  ap.add_argument('-b', '--boostDiMuon', type=float,   dest='boostDiMuon',  default=1., help="boost Di-muon branching ratios")
-  ap.add_argument('-X', '--boostFactor', type=float,   dest='boostFactor',  default=1., help="boost Di-muon prod cross sections")
+  ap.add_argument('-b', '--boostDiMuon', type=float,   dest='boostDiMuon',  default=boostDiMuon, help="boost Di-muon branching ratios")
+  ap.add_argument('-X', '--boostFactor', type=float,   dest='boostFactor',  default=boostFactor, help="boost Di-muon prod cross sections")
+  ap.add_argument('-C', '--charm',      action='store_true',  dest='charm',  default=charm, help="generate charm decays")
+  ap.add_argument('-B', '--beauty',     action='store_true',  dest='beauty', default=beauty, help="generate beauty decays")
+  ap.add_argument('-I', '--inputFile',  action='store_true',  dest='charmInputFile',  default=charmInputFile, help="input file for charm/beauty decays")
+  ap.add_argument('-s', '--nStart',  action='store_true',  dest='nStart',  default=nStart, help="first event of input file if any")
+  ap.add_argument('-M', '--storeOnlyMuons',  action='store_true',  dest='storeOnlyMuons',  default=storeOnlyMuons, help="store only muons, ignore neutrinos")
   ap.add_argument('-o', '--output', type=str, help="output directory", dest='work_dir', default=None)
   args = ap.parse_args()
   if args.debug:
@@ -65,8 +80,28 @@ def init():
   ecut         = args.ecut
   tauOnly      = args.tauOnly
   JpsiMainly   = args.JpsiMainly
+  G4only       = args.G4only
+  boostFactor  = args.boostFactor
+  boostDiMuon  = args.boostDiMuon
+  storeOnlyMuons = args.storeOnlyMuons
+  charm  = args.charm
+  beauty = args.beauty
+  if charm and beauty: 
+    logger.warn("charm and beauty decays are set! Beauty gets priority")
+    charm = False
+  charmInputFile = args.charmInputFile
+  nStart = args.nStart
+  Debug  = args.debug
+  if args.pythiaDecay:
+    withEvtGen = False
+    logger.info("use Pythia8 as primary decayer")
+  else:
+    withEvtGen = True
+    logger.info("use EvtGen as primary decayer") 
   if args.work_dir is None:
-    args.work_dir = get_work_dir(runnr)
+    if charm: args.work_dir = get_work_dir(runnr,"charm")
+    if charm: args.work_dir = get_work_dir(runnr,"beauty")
+    else: args.work_dir = get_work_dir(runnr)
   work_dir = args.work_dir
   logger.debug("work_dir: %s" % work_dir)
   logger.debug("command line arguments: %s", args)
@@ -86,7 +121,6 @@ def init():
   return args
 
 args = init()
-
 os.chdir(work_dir)
 # -------------------------------------------------------------------
 
@@ -131,7 +165,7 @@ MuonShield.SetSupports(False) # otherwise overlap with sensitive Plane
 run.AddModule(MuonShield) # needs to be added because of magn hadron shield.
 sensPlane = ROOT.exitHadronAbsorber()
 sensPlane.SetEnergyCut(ecut*u.GeV) 
-sensPlane.SetOnlyMuons()
+if storeOnlyMuons: sensPlane.SetOnlyMuons()
 # sensPlane.SetZposition(0.*u.cm) # if not using automatic positioning behind default magnetized hadron absorber
 run.AddModule(sensPlane)
 
@@ -142,10 +176,20 @@ P8gen.SetTarget("/TargetArea_1",0.,0.) # will distribute PV inside target, beam 
 P8gen.SetMom(400.*u.GeV)
 P8gen.SetEnergyCut(ecut*u.GeV)
 if G4only: P8gen.SetG4only()
+if withEvtGen: P8gen.WithEvtGen()
 if boostDiMuon > 1:
  P8gen.SetBoost(boostDiMuon) # will increase BR for rare eta,omega,rho ... mesons decaying to 2 muons in Pythia8
                             # and later copied to Geant4
 P8gen.SetSeed(theSeed)
+# for charm/beauty
+#        print ' for experts: p pot= number of protons on target per spill to normalize on'
+#        print '            : c chicc= ccbar over mbias cross section'
+charm = True
+if charm or beauty:
+ pot = 5.E13
+ nStart = 0
+ P8gen.InitForCharmOrBeauty("root://eoslhcb.cern.ch//eos/ship/data/Charm/Cascade-parp16-MSTP82-1-MSEL4-76Mpot_1.root",pot,nStart)
+# P8gen.SetDebug(ROOT.kTRUE)
 primGen.AddGenerator(P8gen)
 #
 run.SetGenerator(primGen)
@@ -187,7 +231,11 @@ tmpFile = outFile+"tmp"
 fin   = ROOT.gROOT.GetListOfFiles()[0]
 fHeader = fin.FileHeader
 fHeader.SetRunId(runnr)
-fHeader.SetTitle("POT = "+str(nev))
+if charm or beauty:
+ poteq = P8gen.GetPotForCharm()
+ fHeader.SetTitle("POT equivalent = %7F.1"%(poteq))
+else: 
+ fHeader.SetTitle("POT = "+str(nev))
 t     = fin.cbmsim
 fout  = ROOT.TFile(tmpFile,'recreate' )
 sTree = t.CloneTree(0)
--- shipgen/FixedTargetGenerator.cxx
+++ shipgen/FixedTargetGenerator.cxx
@@ -13,6 +13,8 @@
 #include "Pythia8Plugins/EvtGen.h"
 #include "EvtGenBase/EvtSimpleRandomEngine.hh"
 #include "EvtGenBase/EvtRandom.hh"
+#include "TDatabasePDG.h"
+#include "TMCProcess.h"
 
 const Double_t cm = 10.; // pythia units are mm
 const Double_t c_light = 2.99792458e+10; // speed of light in cm/sec (c_light   = 2.99792458e+8 * m/s)
@@ -37,37 +39,109 @@ FixedTargetGenerator::FixedTargetGenerator()
   EMax = 0;
   fBoost = 1.;
   withEvtGen = kFALSE;
-  fPythiaN =  new Pythia8::Pythia();
-  fPythiaP =  new Pythia8::Pythia();
+  chicc=1.7e-3;     //prob to produce primary ccbar pair/pot
+  chibb=1.6e-7;     //prob to produce primary bbbar pair/pot
+  setByHand = kFALSE;
+  Debug = kFALSE;
+  firstTime = kTRUE;
+  Option = "Primary";
 }
-// -------------------------------------------------------------------------
+Bool_t FixedTargetGenerator::InitForCharmOrBeauty(TString fInName, Double_t npot, Int_t nStart)
+{
+  Option = "charm";
+  nrpotspill = npot;
+  nEntry = nStart;
+  // open input file with charm or beauty
+  fin   = TFile::Open(fInName);
+  nTree = (TNtuple*)fin->FindObjectAny("pythia6"); // old format, simple ntuple
+  nTree->SetBranchAddress("id",&n_id);
+  nTree->SetBranchAddress("px",&n_px);
+  nTree->SetBranchAddress("py",&n_py);
+  nTree->SetBranchAddress("pz",&n_pz);
+  nTree->SetBranchAddress("M",&n_M);
+  nTree->SetBranchAddress("E",&n_E);
+  nTree->SetBranchAddress("mid",&n_mid);
+  nTree->SetBranchAddress("mpx",&n_mpx);
+  nTree->SetBranchAddress("mpy",&n_mpy);
+  nTree->SetBranchAddress("mpz",&n_mpz);
+  nTree->SetBranchAddress("mE",&n_mE);
+  nEvents = nTree->GetEntries();
+  // Calculate weights, for the whole file.
+  // get histogram with number of pot to normalise
+  nrcpot=((TH1F*)fin->Get("2"))->GetBinContent(1)/2.;
+  // pot are counted double, i.e. for each signal, i.e. pot/2.
+  fLogger->Info(MESSAGE_ORIGIN,"Input file: %s   with %i entries, corresponding to nr-pot=%i",fInName.Data(),nEvents,nrcpot);
+  // book hists for Genie neutrino momentum distribution
+  TDatabasePDG* PDG = TDatabasePDG::Instance();
+  for(Int_t idnu=12; idnu<18; idnu+=2){
+  // nu or anti-nu
+   for (Int_t idadd=-1; idadd<3; idadd+=2){
+    Int_t idhnu=1000+idnu;
+    Int_t idw=idnu;
+    if (idadd==-1){
+     idhnu+=1000;
+     idw=-idnu;
+    }
+    TString name=PDG->GetParticle(idw)->GetName();
+    TString title = name;title+=" momentum (GeV)";
+    TString key = "";key+=idhnu;
+    TH1D* Hidhnu = new TH1D(key,title,400,0.,400.);
+    title = name;title+="  log10-p vs log10-pt";
+    key = "";key+=idhnu+100;
+    TH2D* Hidhnu100 = new TH2D(key,title,100,-0.3,1.7,100,-2.,0.5);
+    title = name;title+="  log10-p vs log10-pt";
+    key = "";key+=idhnu+200;
+    TH2D* Hidhnu200 = new TH2D(key,title,25,-0.3,1.7,100,-2.,0.5);
+   }
+  }
+  wspill = 1.; // event weight == 1 for primary events
+  pot=0.;
+  //Determine fDs on this file for primaries
+  nDsprim=0;
+  ntotprim=0;
 
-// -----   Default constructor   -------------------------------------------
+  return kTRUE;
+}
+// -----   Default Init   -------------------------------------------
 Bool_t FixedTargetGenerator::Init() 
 {
+  fPythiaP =  new Pythia8::Pythia();
+  if (Option == "Primary"){
+   fPythiaN =  new Pythia8::Pythia();
+  }else if (Option == "charm" || Option == "beauty") {
+   fPythiaP =  new Pythia8::Pythia();
+  } else {
+   fLogger->Error(MESSAGE_ORIGIN,"Option not known %s, abort",Option);
+  }
   if (fUseRandom1) fRandomEngine = new PyTr1Rng();
   if (fUseRandom3) fRandomEngine = new PyTr3Rng();
-  fPythiaP->settings.mode("Beams:idB",  2212);
-  fPythiaN->settings.mode("Beams:idB",  2112);
-  std::array<Pythia8::Pythia*,2> plist = {fPythiaP,fPythiaN};
   std::vector<int> s = { 211,321,130,310,3122,3112,3312 };
   std::vector<int> r = { 221, 223,   113, 331, 333};
   std::vector<int> c = { 7, 7, 5, 6, 9}; // decay channel mumu
+  if (Option == "Primary"){
+   fPythiaP->settings.mode("Beams:idB",  2212);
+   fPythiaN->settings.mode("Beams:idB",  2112);
+  }else{
+   fPythiaP->readString("ProcessLevel:all = off");
+  }
+  std::array<Pythia8::Pythia*,2> plist = {fPythiaP,fPythiaN};
+  Int_t pcount = 0;
   for(const auto& fPythia : plist) {
+   if (pcount == 1 && Option != "Primary"){continue;}
+   pcount+=1; 
    fPythia->setRndmEnginePtr(fRandomEngine);
    fPythia->settings.mode("Random:seed",fSeed);
-
-   fPythia->settings.mode("Beams:idA",  2212);
-   fPythia->settings.mode("Beams:frameType",  2);
-   fPythia->settings.parm("Beams:eA",fMom);
-   fPythia->settings.parm("Beams:eB",0.);
-
    fPythia->readString("Next:numberCount = 100000");
-   fPythia->readString("SoftQCD:inelastic = on");
-   fPythia->readString("PhotonCollision:gmgm2mumu = on");
-   fPythia->readString("PromptPhoton:all = on");
-   fPythia->readString("WeakBosonExchange:all = on");
-// http://home.thep.lu.se/~torbjorn/pythia81html/ParticleDecays.html
+   if (Option == "Primary"){
+    fPythia->settings.mode("Beams:idA",  2212);
+    fPythia->settings.mode("Beams:frameType",  2);
+    fPythia->settings.parm("Beams:eA",fMom);
+    fPythia->settings.parm("Beams:eB",0.);
+    fPythia->readString("SoftQCD:inelastic = on");
+    fPythia->readString("PhotonCollision:gmgm2mumu = on");
+    fPythia->readString("PromptPhoton:all = on");
+    fPythia->readString("WeakBosonExchange:all = on");
+   }
    if (tauOnly){
     fPythia->readString("431:new  D_s+  D_s-  1   3   0    1.96849    0.00000    0.00000    0.00000  1.49900e-01   0   1   0   1   0");
     fPythia->readString("431:addChannel = 1   0.0640000    0      -15       16");
@@ -76,7 +150,6 @@ Bool_t FixedTargetGenerator::Init()
     fPythia->readString("443:new  J/psi  J/psi  4   0   0    3.09692    0.00009    3.09602    3.09782  0.   1   1   0   1   0");
     fPythia->readString("443:addChannel = 1   0.1    0      -13       13");
    }
-  
 // make pions/kaons/lambda stable  
    for (unsigned int i=0; i<s.size(); ++i) {
     string particle = std::to_string(s[i])+":mayDecay = false";
@@ -114,11 +187,12 @@ Bool_t FixedTargetGenerator::Init()
    EvtRandom::setRandomEngine(eng);
    EvtGen *myEvtGenPtr = new EvtGen(DecayFile.Data(), ParticleFile.Data(),eng, fsrPtrIn, &models, 1, false);
    TString UdecayFile    = getenv("FAIRSHIP");UdecayFile +="/gconfig/USERDECAY.DEC";
-   //myEvtGenPtr->readUDecay(UdecayFile.Data());
-   // use one instance of EvtGen, requires patch to Pythia8Plugins/EvtGen.h
    evtgenP = new EvtGenDecays(fPythiaP, DecayFile.Data(), ParticleFile.Data(),myEvtGenPtr);
    evtgenP->readDecayFile(UdecayFile.Data()); // will make update of EvtGen with user decay file
-   evtgenN = new EvtGenDecays(fPythiaN, DecayFile.Data(), ParticleFile.Data(),myEvtGenPtr);
+   // use one instance of EvtGen, requires patch to Pythia8Plugins/EvtGen.h
+   if (Option == "Primary"){
+    evtgenN = new EvtGenDecays(fPythiaN, DecayFile.Data(), ParticleFile.Data(),myEvtGenPtr);
+   }
   }
   if (targetName!=""){
    fMaterialInvestigator = new GenieGenerator();
@@ -198,38 +272,79 @@ Bool_t FixedTargetGenerator::ReadEvent(FairPrimaryGenerator* cpg)
    }
   zinter = zinter*cm;
   }
-  Pythia8::Pythia* fPythia;   
+  Pythia8::Pythia* fPythia;
   if (G4only){
-   cpg->AddTrack(2212,0.,0.,fMom,xOff/cm,yOff/cm,zinter/cm,-1,true,-1.,0.,1.);  
-  }else{
-  if (gRandom->Uniform(0.,1.) < ZoverA){
+   cpg->AddTrack(2212,0.,0.,fMom,xOff/cm,yOff/cm,zinter/cm,-1,kTRUE,-1.,0.,1.);  
+  }else if (Option == "Primary"){
+   if (gRandom->Uniform(0.,1.) < ZoverA ){
     fPythiaP->next();
     if (withEvtGen){evtgenP->decay();}
     fPythia = fPythiaP;
-  }else{
+   }else{
     fPythiaN->next();
     if (withEvtGen){evtgenN->decay();}
     fPythia = fPythiaN;
+   }
+  }else{
+    if (nEntry==nEvents){
+      fLogger->Info(MESSAGE_ORIGIN,"Rewind input file: %i",nEntry);
+      nEntry=0;}
+    nTree->GetEvent(nEntry);
+    nEntry+=1;
+// check if we deal with charm or beauty:
+    if (firstTime){ 
+     if (!setByHand and n_M>5){ 
+      chicc = chibb;
+      fLogger->Info(MESSAGE_ORIGIN,"automatic detection of beauty, configured for beauty");
+      fLogger->Info(MESSAGE_ORIGIN,"bb cross section / mbias %f",chicc);
+     }else{
+      fLogger->Info(MESSAGE_ORIGIN,"cc cross section / mbias %f",chicc);
+     }
+   // convert pot to weight corresponding to one spill of 5e13 pot
+     fLogger->Info(MESSAGE_ORIGIN,"weights: %f p.o.t. per spill",nrpotspill);
+     wspill = nrpotspill*chicc/nrcpot;
+     firstTime = kFALSE;
+    }
+    // sanity check, count number of p.o.t. on input file.
+    Double_t pt=TMath::Sqrt( (n_mpx*n_mpx)+(n_mpy*n_mpy));
+    // every event appears twice, i.e.
+    if (pt<1.e-5 && n_mid==2212){ 
+     pot+=+0.5;
+     ntotprim+=1;}
+    Int_t idabs=int(TMath::Abs(n_id));
+    if (idabs==431){ nDsprim+=1;}
+    fPythiaP->event.reset();
+    fPythiaP->event.append(int(n_id),1,0,0,n_px,n_py,n_pz,n_E,n_M,0.,9.);
+    TMCProcess procID  = kPTransportation;
+    if (n_mid==2212 && (n_mpx*n_mpx+n_mpy*n_mpy)<1E-5) {procID = kPPrimary;} // probably primary and not from cascade
+    cpg->AddTrack(int(n_mid),n_mpx,n_mpy,n_mpz, xOff/cm,yOff/cm,zinter/cm,-1,kFALSE,n_mE,0.,wspill,procID);
+    fPythiaP->next();
+    fPythia = fPythiaP;
   }
-
-  for(Int_t ii=0; ii<fPythia->event.size(); ii++){
+  if (withEvtGen){fPythiaP->moreDecays();} // let the very short lived resonances decay via Pythia8
+  if(Debug){fPythia->event.list();}
+  TMCProcess procID;
+  for(Int_t ii=1; ii<fPythia->event.size(); ii++){
      Double_t  e = fPythia->event[ii].e();
      Double_t  m = fPythia->event[ii].m();
      Double_t pz = fPythia->event[ii].pz();
      Int_t id = fPythia->event[ii].id(); 
-     Bool_t wanttracking=true;
-     if (e-m<EMax || !fPythia->event[ii].isFinal() || pz<0) {wanttracking=false;}
+     Bool_t wanttracking=kTRUE;
+     if (e-m<EMax || !fPythia->event[ii].isFinal() || pz<0) {wanttracking=kFALSE;}
      Double_t z  = fPythia->event[ii].zProd()+zinter;
      Double_t x  = fPythia->event[ii].xProd()+xOff;
      Double_t y  = fPythia->event[ii].yProd()+yOff;
      Double_t tof = fPythia->event[ii].tProd();
      Double_t px = fPythia->event[ii].px();  
      Double_t py = fPythia->event[ii].py();  
-     Int_t im = fPythia->event[ii].mother1();
-     if (ii==0){im = -1;}
-     cpg->AddTrack(id,px,py,pz,x/cm,y/cm,z/cm,im,wanttracking,e,tof,1.);
+     Int_t im = fPythia->event[ii].mother1()-1+1;
+     procID = kPPrimary;
+     if (Option != "Primary"){
+        procID = kPDecay;
+        if (ii==1) {procID  = kPHadronic;}
+     }
+     cpg->AddTrack(id,px,py,pz,x/cm,y/cm,z/cm,im,wanttracking,e,tof,wspill,procID);
     }    
-  }
   return kTRUE;
 }
 // -------------------------------------------------------------------------
