b47a2c52ed2e80fa08bc92f5b24e6493249d75a3
Thomas Ruf <Thomas.Ruf@cern.ch>
2017-08-21 12:11:56 +0200

    new charmdet files by Eric for muflux measurement

--- python/shipDet_conf.py
+++ python/shipDet_conf.py
@@ -98,12 +98,12 @@ def configure(run,ship_geo):
                                                         ship_geo.target.z,ship_geo.hadronAbsorber.z,ship_geo.targetOpt,ship_geo.target.sl)
    
  if ship_geo.targetOpt>10:
-  TargetStation.SetLayerPosMat(ship_geo.target.xy,ship_geo.target.L1,ship_geo.target.M1,ship_geo.target.L2,ship_geo.target.M2,\
-  ship_geo.target.L3,ship_geo.target.M3,ship_geo.target.L4,ship_geo.target.M4,ship_geo.target.L5,ship_geo.target.M5,\
-  ship_geo.target.L6,ship_geo.target.M6,ship_geo.target.L7,ship_geo.target.M7,ship_geo.target.L8,ship_geo.target.M8,\
-  ship_geo.target.L9,ship_geo.target.M9,ship_geo.target.L10,ship_geo.target.M10,ship_geo.target.L11,ship_geo.target.M11,\
-  ship_geo.target.L12,ship_geo.target.M12,ship_geo.target.L13,ship_geo.target.M13,ship_geo.target.L14,ship_geo.target.M14,\
-  ship_geo.target.L15,ship_geo.target.M15,ship_geo.target.L16,ship_geo.target.M16,ship_geo.target.L17,ship_geo.target.M17)
+  slices_length   = ROOT.std.vector('float')()
+  slices_material = ROOT.std.vector('std::string')()
+  for i in range(1,ship_geo.targetOpt+1):
+   slices_length.push_back(  eval("ship_geo.target.L"+str(i)))
+   slices_material.push_back(eval("ship_geo.target.M"+str(i)))
+  TargetStation.SetLayerPosMat(ship_geo.target.xy,slices_length,slices_material)
  detectorList.append(TargetStation)
 
  if ship_geo.muShieldDesign==1:
