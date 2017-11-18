d1849c3ef810e2b66d0abb10a188e68e7633f09d
John Back <J.J.Back@warwick.ac.uk>
2017-11-18 03:05:55 +0100

    Correction for checking non-zero rotation angles in ShipBFieldMap::initialise.

--- field/ShipBFieldMap.cxx
+++ field/ShipBFieldMap.cxx
@@ -176,7 +176,7 @@ void ShipBFieldMap::initialise()
 	if (isCopy_ == kFALSE) {this->readMapFile();}
 
 	// Set the global co-ordinate translation and rotation info
-	if (fabs(phi_) > 1e-6 && fabs(theta_) > 1e-6 && fabs(psi_) > 1e-6) {
+	if (fabs(phi_) > 1e-6 || fabs(theta_) > 1e-6 || fabs(psi_) > 1e-6) {
 
 	    // We have non-zero rotation angles. Create a combined translation and rotation
 	    TGeoTranslation tr("offsets", xOffset_, yOffset_, zOffset_);
