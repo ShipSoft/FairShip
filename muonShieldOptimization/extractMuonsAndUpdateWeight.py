afcf7c14c6c3bbdfb42782b8fb8ca8a8d426d70c
Thomas Ruf <Thomas.Ruf@cern.ch>
2018-04-10 15:43:55 +0200

    update tools used for background production

--- muonShieldOptimization/extractMuonsAndUpdateWeight.py
+++ muonShieldOptimization/extractMuonsAndUpdateWeight.py
@@ -9,21 +9,27 @@ charmExtern = [4332,4232,4132,4232,4122,431,411,421]
 
 muSourcesIDs = muSources.values()
 
-# for 10GeV Yandex Production 65.041 Billion PoT, weight 768.75 for 5E13 pot
-weight = 768.75 
+# for 10GeV Yandex Production 65.041 Billion PoT, weight = 768.75 for 5E13 pot
+weightMbias = 768.75
 
-def muonUpdateWeight(sTree,diMuboost,xSecboost):
+# for 10GeV charm Production 102.2 Billion PoT equivalent, weight = 489.24
+# added another cycle
+weightCharm = 489.24 * 2./3. 
+
+# for 10GeV charm Production 5336 Billion PoT equivalent, weight = 9.37
+weightBeauty = 9.37
+
+def muonUpdateWeight(sTree,diMuboost,xSecboost,noCharm=True):
  nMu=0
  for v in sTree.vetoPoint:
-   v = sTree.vetoPoint[0]
    mu = v.GetTrackID()
    t = sTree.MCTrack[mu]
    if abs(t.GetPdgCode())!=13: continue
    moID  = abs(sTree.MCTrack[t.GetMotherId()].GetPdgCode())
-   if moID in charmExtern: continue  # take heavy flavours from separate production
+   if moID in charmExtern and noCharm: continue  # take heavy flavours from separate production
    nMu+=1
    pName = t.GetProcName().Data()
-   t.MultiplyWeight(weight)
+   t.SetWeight(weight)
    if not pName.find('Hadronic inelastic')<0:
      t.MultiplyWeight(1./diMuboost)
    elif not pName.find('Lepton pair')<0:
@@ -45,7 +51,9 @@ def PoT(f):
   if k.GetName() == 'FileHeader':
    txt = k.GetTitle()
    tmp = txt.split('=')[1]
-   nTot += int(tmp.split('with')[0])
+   tmp2 = tmp.split('with')[0]
+   if tmp2.find('E')<0: nTot += int(tmp2)
+   else: nTot += float(tmp2)
    ncycles+=1
    i = txt.find('diMu')
    if i < 0:
@@ -71,7 +79,7 @@ def TotStat():
   ntot += nPot
  print "Total statistics so far",ntot/1.E9," billion" 
 
-def processFile(fin):
+def processFile(fin,noCharm=True):
     f   = ROOT.TFile.Open(os.environ['EOSSHIP']+path+fin)
     nPot,diMuboost,xSecboost  = PoT(f)
     sTree = f.cbmsim
@@ -80,7 +88,7 @@ def processFile(fin):
     newTree = sTree.CloneTree(0)
     for n in range(sTree.GetEntries()):
       sTree.GetEntry(n)
-      nMu = muonUpdateWeight(sTree,diMuboost,xSecboost)
+      nMu = muonUpdateWeight(sTree,diMuboost,xSecboost,noCharm)
       if nMu>0:
        rc = newTree.Fill()
     ff = f.FileHeader.Clone('Extracted Muon Background File')
@@ -96,6 +104,8 @@ def processFile(fin):
 
 def run():
  tmp = "pythia8_Geant4_10.0_cXX.root"
+ global weight 
+ weight = weightMbias 
  for run in range(0,67000,1000):
    rc = processFile(tmp.replace('XX',str(run)))
    if rc == 0:
@@ -109,11 +119,13 @@ def run():
 
 def run4Charm():
  tmp = "pythia8_Geant4_charm_XX-YY_10.0.root"
- for cycle in [0,1]:
+ global weight
+ weight = weightCharm
+ for cycle in [0,1,2]:
   for run in range(0,100,20):
-   crun = run+cyle*1000
+   crun = run+cycle*1000
    fname = tmp.replace('XX',str(crun)).replace('YY',str(crun+19))
-   rc = processFile(fname)
+   rc = processFile(fname,False)
    if rc == 0:
      fmu = fname.replace('.root',"_mu.root")
      rc = os.system("xrdcp "+fmu+" $EOSSHIP/eos/experiment/ship/data/Mbias/background-prod-2018/"+fmu)
@@ -121,4 +133,138 @@ def run4Charm():
       print "copy to EOS failed, stop",fmu
      else:
       rc = os.system("rm "+fmu)
+
+def run4beauty():
+ global weight
+ weight = weightBeauty
+ fname = "pythia8_Geant4_beauty_5336B_10.0.root"
+ rc = processFile(fname,False)
+ if rc == 0:
+     fmu = fname.replace('.root',"_mu.root")
+     rc = os.system("xrdcp "+fmu+" $EOSSHIP/eos/experiment/ship/data/Mbias/background-prod-2018/"+fmu)
+     if rc != 0: 
+      print "copy to EOS failed, stop",fmu
+     else:
+      rc = os.system("rm "+fmu)
+
+# merge with beauty in a second step
+
+
+# finished one file pythia8_Geant4_10.0_withCharm44000_mu.root 2712798 1113638
+
+def mergeCharm():
+ tmp = "pythia8_Geant4_charm_XX-YY_10.0.root"
+ mergedFile = "pythia8_Geant4_charm_102.2B_10.0_mu.root"
+ cmd = "hadd "+mergedFile
+ for cycle in [0,1]:
+  for run in range(0,100,20):
+   crun = run+cycle*1000
+   fname = tmp.replace('XX',str(crun)).replace('YY',str(crun+19))
+   fmu = " $EOSSHIP/eos/experiment/ship/data/Mbias/background-prod-2018/"+fname.replace('.root',"_mu.root")
+   cmd += fmu
+ rc = os.system(cmd)
+ rc = os.system("xrdcp "+mergedFile+" $EOSSHIP/eos/experiment/ship/data/Mbias/background-prod-2018/"+mergedFile)
+
+def mergeMbiasAndCharm(flavour="charm"):
+ done = []
+ timer = ROOT.TStopwatch()
+ timer.Start()
+ pp = os.environ['EOSSHIP']+path
+ Rndm=ROOT.TRandom3()
+ Rndm.SetSeed(0)
+ if flavour=="charm": 
+   allFiles = {'charm':"pythia8_Geant4_charm_102.2B_10.0_mu.root"}
+   tmp = "pythia8_Geant4_10.0_cXX_mu.root"
+ else:                
+   allFiles = {'beauty':"pythia8_Geant4_beauty_5336B_10.0_mu.root"}
+   tmp = "pythia8_Geant4_10.0_withCharmXX_mu.root"
+ for run in range(0,67000,1000):
+    allFiles[str(run)] = tmp.replace('XX',str(run))
+ nEntries = {}
+ Nall = 0
+ for x in allFiles:
+  f=ROOT.TFile.Open(pp+allFiles[x])
+  nEntries[x]=f.cbmsim.GetEntries()
+  Nall += nEntries[x]
+ nCharm = 0
+ nDone  = 0
+ frac = nEntries[flavour]/float(Nall)
+ print "debug",frac
+ os.system('xrdcp '+pp +allFiles[flavour] +' '+allFiles[flavour])
+ for k in allFiles:
+  if k==flavour: continue
+  if k in done:  continue
+  os.system('xrdcp '+pp +allFiles[k] +' ' +allFiles[k])
+  os.system('hadd -f tmp.root '+ allFiles[flavour] + ' '+ allFiles[k] )
+  os.system('rm '+allFiles[k])
+  f = ROOT.TFile('tmp.root')
+  sTree = f.cbmsim
+  sTree.LoadBaskets(30000000000)
+  if flavour=="charm": 
+   outFile = tmp.replace('cXX','withCharm'+k)
+  else:
+   outFile = tmp.replace('XX','andBeauty'+k)
+  fmu = ROOT.TFile(outFile,"recreate")
+  newTree = sTree.CloneTree(0)
+  nMbias = 0
+ # 0 - nEntries['charm']-1 , nEntries['charm'] - nEntries[0]-1, nEntries[0] - nEntries[1000]-1
+  # randomList = ROOT.TEntryList(chain)
+  myList = []
+  while nMbias<nEntries[k]:
+   copyEvent = True
+   if Rndm.Rndm() > frac:
+    # rc = randomList.Enter(nMbias+nEntries['charm'])
+    myList.append( nMbias+nEntries[flavour] )
+    nMbias+=1
+   else:
+    if nEntries[flavour]>nCharm:
+     # rc = randomList.Enter(nCharm)
+     myList.append( nCharm )
+     nCharm+=1
+    else: copyEvent = False
+  # chain.SetEntryList(randomList) 
+  # nev = randomList.GetN()
+  nev = len(myList)
+  print "start:",outFile,nev
+  for iev in range(nev) :
+     rc =sTree.GetEntry(myList[iev])
+     rc = newTree.Fill()
+     if (iev)%100000==0:
+       timer.Stop()
+       print "status:",timer.RealTime(),k,iev
+       timer.Start()
+  newTree.AutoSave()
+  print "finished one file",outFile,nMbias,nCharm
+  if flavour=="charm": 
+   ff = f.FileHeader.Clone('With Charm Merged Muon Background File')
+  else: 
+   ff = f.FileHeader.Clone('With Charm and Beauty Merged Muon Background File')
+  txt = ff.GetTitle()
+  fmu.cd()
+  ff.Write("FileHeader", ROOT.TObject.kSingleKey)
+  fmu.Close()
+  f.Close()
+  rc = os.system("xrdcp "+outFile+" $EOSSHIP/eos/experiment/ship/data/Mbias/background-prod-2018/"+outFile)
+  if rc != 0: 
+      print "copy to EOS failed",outFile
+  else:
+      rc = os.system("rm "+outFile)
+
+def testRatio(fname):
+ f=ROOT.TFile.Open(os.environ['EOSSHIP']+path+fname)
+ sTree = f.cbmsim
+ Nall = sTree.GetEntries()
+ charm = 0
+ for n in range(Nall):
+   rc = sTree.GetEvent(n)
+   for m in sTree.MCTrack:
+    pdgID = abs(m.GetPdgCode())
+    if pdgID in charmExtern:
+        charm+=1
+        break
+ print "charm found",charm," ratio ",charm/float(Nall)
+ 
+
+
+
 
---
+++ muonShieldOptimization/runCharmHadProd.py
@@ -0,0 +1,163 @@
+import sys,os,ROOT
+ncpus = 20
+msel = "4"
+# msel = "5"
+
+if msel == "4":
+ nev   = 2000000  # 0.1s / event
+ path  = "/afs/cern.ch/project/lbcern/vol1/truf/charm/"
+else:
+ nev   = 1000000 
+ path  = "/afs/cern.ch/project/lbcern/vol1/truf/beauty/"
+
+# path = "/home/truf/charm/"
+
+# store only charm
+
+# story about weights, run_fixedTarget puts into file header: pot = nrpotspill / wspill
+# with wspill = nrpotspill*chicc/nrcpot*nEvents/nev -> pot =  nev * nrcpot / (chicc * nEvents)
+# nEvents nr of events in input file, number of events requested for job, ratio should be 1
+# pot =  nrcpot / chicc 
+# nrcpot=((TH1F*)fin->Get("2"))->GetBinContent(1)/2.;  // pot are counted double, i.e. for each signal, i.e. pot/2.
+
+def makeHadrons(run):
+ for n in range(ncpus):
+  s = x.Rndm()*1000000000.
+  os.system('mkdir run'+str(run))
+  os.chdir('run'+str(run))
+  cmd = "python $FAIRSHIP/macro/makeCascade.py -m "+msel+" -n " + str(nev) + " -t  Cascade-run"+str(run)+"-parp16-MSTP82-1-MSEL"+msel+".root"
+  # if not run in runList: 
+  os.system(cmd+ " >log"+str(run)+" &")
+  os.chdir('../')
+  run+=1
+
+def makeBackground(run,cycle=0):
+ for n in range(ncpus):
+  orun = run+cycle*1000
+  os.chdir('run'+str(run))
+  inputFile = path+"run"+str(run)+"/Cascade-run"+str(run)+"-parp16-MSTP82-1-MSEL"+msel+".root"
+  f=ROOT.TFile(inputFile)
+  nt = f.pythia6
+  N = nt.GetEntries()
+  f.Close()
+  if msel == "4":
+   cmd = "python $FAIRSHIP/muonShieldOptimization/run_fixedTarget.py --force --charm -V -e 10 -P -n "+str(N)+" -r "+str(orun)+" -b 100 -X 100 -I "+inputFile
+  else:
+   cmd = "python $FAIRSHIP/muonShieldOptimization/run_fixedTarget.py --force --beauty -V -e 10 -P -n "+str(N)+" -r "+str(orun)+" -b 100 -X 100 -I "+inputFile
+  os.system(cmd+ " >logFT"+str(orun)+" &")
+  os.chdir('../')
+  run+=1
+
+cycle = 2
+runList = [20,28,30,33]
+def makeBackgroundX(runList,cycle=0):
+ for run in runList:
+  orun = run+cycle*1000
+  os.chdir('run'+str(run))
+  inputFile = path+"run"+str(run)+"/Cascade-run"+str(run)+"-parp16-MSTP82-1-MSEL"+msel+".root"
+  f=ROOT.TFile(inputFile)
+  nt = f.pythia6
+  N = nt.GetEntries()
+  f.Close()
+  cmd = "python $FAIRSHIP/muonShieldOptimization/run_fixedTarget.py --force --charm -V -e 10 -P -n "+str(N)+" -r "+str(orun)+" -b 100 -X 100 -I "+inputFile
+  if run in runList: os.system(cmd+ " >logFT"+str(orun)+" &")
+  os.chdir('../')
+
+def merge(run,cycle=0):
+ fname = "pythia8_Geant4_XX_10.0.root"
+ cmd = " " 
+ for n in range(ncpus):
+  for x in os.listdir(path+'/run'+str(run+n)):
+   orun = run+cycle*1000
+   if not x.find('run_fixedTarget_'+str(orun+n)) < 0:
+     if cycle == 0 and run == 0 and not x.find('1001') < 0: continue
+     if cycle == 0 and run == 0 and not x.find('1010') < 0: continue
+     cmd += path+'/run'+str(run+n)+'/'+x+'/'+fname.replace('XX',str(orun+n))+' '
+ if msel == "4": outFile = fname.replace('XX', 'charm_'+str(orun)+'-'+str(orun+ncpus-1) )
+ else:           outFile = fname.replace('XX', 'beauty_'+str(orun)+'-'+str(orun+ncpus-1) )
+ rc = os.system("hadd -O "+outFile + " " +cmd)
+ if rc != 0: 
+    print "hadd failed, stop",outFile 
+ else:
+   rc = os.system("xrdcp "+outFile+" $EOSSHIP/eos/experiment/ship/data/Mbias/background-prod-2018/"+outFile)
+   if rc != 0: 
+    print "copy to EOS failed, stop",outFile 
+   else:
+    rc = os.system("rm "+outFile)
+
+def mergeAll():
+ cmd = "hadd pythia8_Geant4_charm_153.3B_10.0_mu.root "
+ tmp = "/eos/experiment/ship/data/Mbias/background-prod-2018/pythia8_Geant4_charm_XX_10.0_mu.root" 
+ for x in ["0-19","20-39","40-59","60-79","80-99",\
+           "1000-1019","1020-1039","1040-1059","1060-1079","1080-1099",\
+           "2000-2019","2020-2039","2040-2059","2060-2079","2080-2099"]:
+  cmd+=tmp.replace('XX',x)+" "
+ os.system(cmd)
+
+def compactifyCascade(run):
+ ncpus = 20
+ cmd = ''
+ Ntot = 0
+ NperJob = nev
+ for i in range(run,+ncpus):
+   fName = path+"run"+str(i)+"/Cascade-run"+str(i)+"-parp16-MSTP82-1-MSEL"+msel+".root"
+   f=open(path+"run"+str(i)+"/log"+str(i))
+   success = False
+   for l in f.readlines():
+    if not l.find('Macro finished succesfully')<0: success = True
+   if not success:
+     print "job not finished properly",fName
+     continue  
+   cmd += fName +" "
+   f.close()
+   Ntot+= NperJob
+ if cmd.find('root')<0:
+  print 'no file found, exit'
+ else:
+  stat = str( int(Ntot/1E6))+'Mpot'
+  outFile = "Cascade-run"+str(run)+"-"+str(run+ncpus-1)+"-parp16-MSTP82-1-MSEL"+msel+"-"+stat+".root"
+  rc = os.system("hadd -O "+outFile + " " +cmd)
+  f = ROOT.TFile(outFile) 
+  Npot = f.Get("2").GetBinContent(1)/2./chicc
+  f.Close()
+  stat = str( int(Npot/1E9))+'Bpot'
+  oldOutFile = outFile
+  outFile = "Cascade-run"+str(run)+"-"+str(run+ncpus-1)+"-parp16-MSTP82-1-MSEL"+msel+"-"+stat+".root"
+  os.system("mv "+oldOutFile+" "+outFile)
+  rc = os.system("xrdcp "+outFile+" $EOSSHIP/eos/experiment/ship/data/Mbias/background-prod-2018/"+outFile)
+  if rc != 0: 
+    print "copy to EOS failed, stop",outFile 
+  else:
+    rc = os.system("rm "+outFile)
+
+def statistics():
+ chicc=1.7e-3
+ if msel=='5': chicc = 1.6e-7
+ path = os.environ['EOSSHIP']+"/eos/experiment/ship/data/Mbias/background-prod-2018/"
+ fname =  "Cascade-runAA-BB-parp16-MSTP82-1-MSEL4-40Mpot.root"
+ nPot = 0
+ nhadrons = 0
+ for x in [0,20,40,60,80]:
+  fn = fname.replace('AA',str(x)).replace('BB',str(x+19))
+  f=ROOT.TFile.Open(path+fn)
+  nPot += f.Get("2").GetBinContent(1)/2.
+  nhadrons += f.Get('pythia6').GetEntries()
+ print "total nr of hadrons:",nhadrons,nPot/chicc/1.E9,'Billion'
+
+def potFromFileHeader():
+ pot = 0
+ for x in f.GetListOfKeys():
+  if x.GetName()=='FileHeader':
+   pot += float(x.GetTitle().split(' ')[3])
+ print "PoT = ",pot
+
+x=ROOT.TRandom3()
+x.SetSeed(0)
+
+run = int(sys.argv[1])
+
+print "following functions exist"
+print " - makeHadrons(run): will run makeCascade"
+print " - makeBackground(run): will run fixedTarget generator"
+
+
