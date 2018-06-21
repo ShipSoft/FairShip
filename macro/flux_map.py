275d1bd655ac4270934ed7a8b1e1cad500c9dde4
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-06-21 10:16:21 +0200

    Correct weights, update detectors

--- macro/flux_map.py
+++ macro/flux_map.py
@@ -15,18 +15,12 @@ def main():
         help='''Simulation results to use as input. '''
         '''Supports retrieving files from EOS via the XRootD protocol.''')
     parser.add_argument(
-        'geofile',
-        help='''Geometry file to use. '''
-        '''Supports retrieving files from EOS via the XRootD protocol.''')
-    parser.add_argument(
         '-o',
         '--outputfile',
         default='flux_map.root',
         help='''File to write the flux maps to. '''
         '''Will be recreated if it already exists.''')
     args = parser.parse_args()
-    g = r.TFile.Open(args.geofile, 'read')
-    sGeo = g.FAIRGeom
     f = r.TFile.Open(args.outputfile, 'recreate')
     f.cd()
     maxpt = 10. * u.GeV
@@ -47,21 +41,22 @@ def main():
         ut.bookHist(h, 'timing_{}'.format(suffix),
                     '{};x[cm];y[cm]'.format(title), 3, -252, +252, 167, -501,
                     501)
-        ut.bookHist(h, 'UVT_{}'.format(suffix), '{};x[cm];y[cm]'.format(title),
-                    100, -300, +300, 100, -300, 300)
-        ut.bookHist(h, 'NuTauMu_{}'.format(suffix), '{};x[cm];y[cm]'.format(title),
-                    100, -300, +300, 100, -300, 300)
+        ut.bookHist(h, 'NuTauMu_{}'.format(suffix),
+                    '{};x[cm];y[cm]'.format(title), 100, -300, +300, 100, -300,
+                    300)
         ut.bookHist(h, 'ECAL_TP_{}'.format(suffix),
                     '{};x[cm];y[cm]'.format(title), 167, -501, +501, 334,
                     -1002, 1002)
         ut.bookHist(h, 'ECAL_Alt_{}'.format(suffix),
                     '{};x[cm];y[cm]'.format(title), 50, -500, +500, 100, -1000,
                     1000)
-        ut.bookHist(h, 'SBT_Liquid_{}'.format(suffix), '{};z[cm];#phi'.format(title),
-                    100, -3000, +3000, 100, -r.TMath.Pi(), r.TMath.Pi())
-        ut.bookHist(h, 'SBT_Plastic_{}'.format(suffix), '{};z[cm];#phi'.format(title),
-                    100, -3000, +3000, 100, -r.TMath.Pi(), r.TMath.Pi())
-        for station in range(1, 6):
+        ut.bookHist(h, 'SBT_Liquid_{}'.format(suffix),
+                    '{};z[cm];#phi'.format(title), 100, -3000, +3000, 100,
+                    -r.TMath.Pi(), r.TMath.Pi())
+        ut.bookHist(h, 'SBT_Plastic_{}'.format(suffix),
+                    '{};z[cm];#phi'.format(title), 100, -3000, +3000, 100,
+                    -r.TMath.Pi(), r.TMath.Pi())
+        for station in range(1, 5):
             ut.bookHist(h, 'T{}_{}'.format(station, suffix),
                         '{};x[cm];y[cm]'.format(title), 10, -250, +250, 20,
                         -500, 500)
@@ -71,8 +66,6 @@ def main():
                     maxpt)
         ut.bookHist(h, 'mu_ppt{}'.format(suffix), '#mu#pm;p[GeV];p_t[GeV];',
                     100, 0, maxp, 100, 0, maxpt)
-    ut.bookHist(h, 'smear', '#mu#pm initial vertex;x[cm];y[cm]', 100, -10, +10,
-                100, -10, 10)
     ut.bookHist(h, 'ECAL_TP_e', 'e#pm with E#geq 250 MeV;x[cm];y[cm]', 167,
                 -501, +501, 334, -1002, 1002)
     ut.bookHist(h, 'ECAL_Alt_e', 'e#pm with E#geq 250 MeV;x[cm];y[cm]', 50,
@@ -93,17 +86,14 @@ def main():
             print '{}/{}'.format(i, n)
         i += 1
         muon = False
-        original_muon = event.MCTrack[1]
-        try:
-            h['smear'].Fill(original_muon.GetStartX(),
-                            original_muon.GetStartY())
-        except AttributeError:
-            continue
         for hit in event.strawtubesPoint:
             if hit:
                 if not hit.GetEnergyLoss() > 0:
                     continue
-                weight = event.MCTrack[hit.GetTrackID()].GetWeight()
+                trid = hit.GetTrackID()
+                weight = event.MCTrack[trid].GetWeight() if trid > 0 else 0
+                if not weight:
+                    continue
                 x = hit.GetX()
                 y = hit.GetY()
                 z = hit.GetZ()
@@ -119,6 +109,7 @@ def main():
                 h['T{}_all'.format(station)].Fill(x, y, weight)
                 if abs(pid) == 13:
                     muon = True
+                    muonid = trid
                     h['T{}_mu'.format(station)].Fill(x, y, weight)
                     h['mu_p'].Fill(P, weight)
                     h['mu_pt'].Fill(pt, weight)
@@ -127,7 +118,10 @@ def main():
             if hit:
                 if not hit.GetEnergyLoss() > 0:
                     continue
-                weight = event.MCTrack[hit.GetTrackID()].GetWeight()
+                trid = hit.GetTrackID()
+                weight = event.MCTrack[trid].GetWeight() if trid > 0 else 0
+                if not weight:
+                    continue
                 x = hit.GetX()
                 y = hit.GetY()
                 px = hit.GetPx()
@@ -142,6 +136,7 @@ def main():
                 h['ECAL_Alt_all'].Fill(x, y, weight)
                 if abs(pid) == 13:
                     muon = True
+                    muonid = trid
                     h['mu_p'].Fill(P, weight)
                     h['mu_pt'].Fill(pt, weight)
                     h['mu_ppt'].Fill(P, pt, weight)
@@ -161,7 +156,10 @@ def main():
             if hit:
                 if not hit.GetEnergyLoss() > 0:
                     continue
-                weight = event.MCTrack[hit.GetTrackID()].GetWeight()
+                trid = hit.GetTrackID()
+                weight = event.MCTrack[trid].GetWeight() if trid > 0 else 0
+                if not weight:
+                    continue
                 x = hit.GetX()
                 y = hit.GetY()
                 z = hit.GetZ()
@@ -175,15 +173,45 @@ def main():
                 h['NuTauMu_all'].Fill(x, y, weight)
                 if abs(pid) == 13:
                     muon = True
+                    muonid = trid
                     h['mu_p'].Fill(P, weight)
                     h['mu_pt'].Fill(pt, weight)
                     h['mu_ppt'].Fill(P, pt, weight)
                     h['NuTauMu_mu'].Fill(x, y, weight)
+        for hit in event.TimeDetPoint:
+            if hit:
+                if not hit.GetEnergyLoss() > 0:
+                    continue
+                trid = hit.GetTrackID()
+                weight = event.MCTrack[trid].GetWeight() if trid > 0 else 0
+                if not weight:
+                    continue
+                x = hit.GetX()
+                y = hit.GetY()
+                z = hit.GetZ()
+                px = hit.GetPx()
+                py = hit.GetPy()
+                pz = hit.GetPz()
+                pt = np.hypot(px, py)
+                P = np.hypot(pz, pt)
+                pid = hit.PdgCode()
+                assert pid not in [12, -12, 14, -14, 16, -16]
+                h['timing_all'].Fill(x, y, weight)
+                if abs(pid) == 13:
+                    muon = True
+                    muonid = trid
+                    h['mu_p'].Fill(P, weight)
+                    h['mu_pt'].Fill(pt, weight)
+                    h['mu_ppt'].Fill(P, pt, weight)
+                    h['timing_mu'].Fill(x, y, weight)
         for hit in event.muonPoint:
             if hit:
                 if not hit.GetEnergyLoss() > 0:
                     continue
-                weight = event.MCTrack[hit.GetTrackID()].GetWeight()
+                trid = hit.GetTrackID()
+                weight = event.MCTrack[trid].GetWeight() if trid > 0 else 0
+                if not weight:
+                    continue
                 x = hit.GetX()
                 y = hit.GetY()
                 px = hit.GetPx()
@@ -198,6 +226,7 @@ def main():
                 h['muon_bars_y_all'].Fill(x, y, weight)
                 if abs(pid) == 13:
                     muon = True
+                    muonid = trid
                     h['mu_p'].Fill(P, weight)
                     h['mu_pt'].Fill(pt, weight)
                     h['mu_ppt'].Fill(P, pt, weight)
@@ -208,7 +237,10 @@ def main():
             if hit:
                 if not hit.GetEnergyLoss() > 0:
                     continue
-                weight = event.MCTrack[hit.GetTrackID()].GetWeight()
+                trid = hit.GetTrackID()
+                weight = event.MCTrack[trid].GetWeight() if trid > 0 else 0
+                if not weight:
+                    continue
                 x = hit.GetX()
                 y = hit.GetY()
                 z = hit.GetZ()
@@ -220,32 +252,12 @@ def main():
                 pid = hit.PdgCode()
                 detector_ID = hit.GetDetectorID()
                 assert pid not in [12, -12, 14, -14, 16, -16]
-                detector = sGeo.FindNode(x, y, z).GetName()
-                # Timing detector:
-                if detector == 'TimeDet_1':
-                    h['timing_all'].Fill(x, y, weight)
-                    if abs(pid) == 13:
-                        muon = True
-                        h['mu_p'].Fill(P, weight)
-                        h['mu_pt'].Fill(pt, weight)
-                        h['mu_ppt'].Fill(P, pt, weight)
-                        h['timing_mu'].Fill(x, y, weight)
-                    continue
-                # UVT
-                if detector == 'VetoTimeDet_1':
-                    h['UVT_all'].Fill(x, y, weight)
-                    if abs(pid) == 13:
-                        muon = True
-                        h['mu_p'].Fill(P, weight)
-                        h['mu_pt'].Fill(pt, weight)
-                        h['mu_ppt'].Fill(P, pt, weight)
-                        h['UVT_mu'].Fill(x, y, weight)
-                    continue
                 phi = r.TMath.ATan2(y, x)
                 if 99999 < detector_ID < 999999:
                     h['SBT_Liquid_all'].Fill(z, phi, weight)
                     if abs(pid) == 13:
                         muon = True
+                        muonid = trid
                         h['mu_p'].Fill(P, weight)
                         h['mu_pt'].Fill(pt, weight)
                         h['mu_ppt'].Fill(P, pt, weight)
@@ -255,6 +267,7 @@ def main():
                     h['SBT_Plastic_all'].Fill(z, phi, weight)
                     if abs(pid) == 13:
                         muon = True
+                        muonid = trid
                         h['mu_p'].Fill(P, weight)
                         h['mu_pt'].Fill(pt, weight)
                         h['mu_ppt'].Fill(P, pt, weight)
@@ -262,7 +275,8 @@ def main():
                     continue
                 print 'Unidentified vetoPoint.'
         if muon:
-            weight = event.MCTrack[1].GetWeight()
+            original_muon = event.MCTrack[muonid]
+            weight = original_muon.GetWeight()
             h['mu_p_original'].Fill(original_muon.GetP(), weight)
             h['mu_pt_original'].Fill(original_muon.GetPt(), weight)
             h['mu_ppt_original'].Fill(original_muon.GetP(),
