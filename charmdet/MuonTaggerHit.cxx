1de6ba28ab1e85d65e00e07e72446f85d16a6310
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-27 13:31:48 +0200

    Fix strip pitch, use centre of strip instead of corners

--- charmdet/MuonTaggerHit.cxx
+++ charmdet/MuonTaggerHit.cxx
@@ -13,6 +13,8 @@ void MuonTaggerHit::EndPoints(TVector3 &vbot, TVector3 &vtop)
    int station = fDetectorID / 10000;
    Direction direction = static_cast<Direction>((fDetectorID % 10000) / 1000);
    int strip = fDetectorID % 1000;
+   const int NR_HORI_STRIPS = 116;    // nr horizontal strips, X dir, Y coord
+   const int NR_VER_STRIPS = 184;     // nr vertical strips, Y dir, X coord
    const float STRIP_XWIDTH = 0.8625; // internal STRIP V, WIDTH, in cm
    const float EXT_STRIP_XWIDTH_L =
       0.9625; // nominal (R&L) and Left measured external STRIP V, WIDTH, in cm (beam along z, out from the V plane)
@@ -20,9 +22,12 @@ void MuonTaggerHit::EndPoints(TVector3 &vbot, TVector3 &vtop)
       0.86; // measured Right external STRIP V, WIDTH, in cm (beam along z, out from the V plane)
    const float STRIP_YWIDTH = 0.8625;  // internal STRIP H, WIDTH, in cm
    const float EXT_STRIP_YWIDTH = 0.3; // measured external STRIP H, WIDTH, in cm (nominal 0.4375)
-   /* gGeoManager-> */                 // Get Z position
-   const float total_width = 182 * STRIP_XWIDTH + EXT_STRIP_XWIDTH_L + EXT_STRIP_XWIDTH_R;
-   const float total_height = 114 * STRIP_YWIDTH + 2 * EXT_STRIP_YWIDTH;
+   const float H_STRIP_OFF = 0.1983;   // offset between adjacent H strips, in cm
+   const float V_STRIP_OFF = 0.2000;   // offset between adjacent V strips, in cm
+   const float total_width =
+      (NR_VER_STRIPS - 2) * STRIP_XWIDTH + EXT_STRIP_XWIDTH_L + EXT_STRIP_XWIDTH_R + (NR_VER_STRIPS - 1) * V_STIP_OFF;
+   const float total_height =
+      (NR_HORI_STRIPS - 2) * STRIP_YWIDTH + 2 * EXT_STRIP_YWIDTH + (NR_HORI_STRIPS - 1) * H_STRIP_OFF;
    const float x_start = (total_width - EXT_STRIP_XWIDTH_R + EXT_STRIP_XWIDTH_L) / 2;
    const float y_start = total_height / 2;
    float Z = 0;
@@ -38,28 +43,32 @@ void MuonTaggerHit::EndPoints(TVector3 &vbot, TVector3 &vtop)
 
    switch (direction) {
    case horizontal:
+      float y;
+      auto xtop = x_start;
+      auto xbot = x_start - total_width;
       if (strip == 1) { // 1st strip (top)
-         vtop.SetXYZ(x_start, y_start, Z);
-         vbot.SetXYZ(x_start - total_width, y_start - EXT_STRIP_YWIDTH, Z);
-      } else if (strip == 116) { // last strip (bottom)
-         vtop.SetXYZ(x_start, y_start - EXT_STRIP_YWIDTH - 114 * STRIP_YWIDTH, Z);
-         vbot.SetXYZ(x_start - total_width, y_start - total_height, Z);
+         y = y_start - EXT_STRIP_YWIDTH / 2;
+      } else if (strip == NR_HORI_STRIPS) { // last strip (bottom)
+         y = y_start - total_height + EXT_STRIP_YWIDTH / 2;
       } else { // all other horizontal strips
-         vtop.SetXYZ(x_start, y_start - EXT_STRIP_YWIDTH - (strip - 2) * STRIP_YWIDTH, Z);
-         vbot.SetXYZ(x_start - total_width, y_start - EXT_STRIP_YWIDTH - (strip - 1) * STRIP_YWIDTH, Z);
+         y = y_start - EXT_STRIP_YWIDTH - (strip - 1.5) * STRIP_YWIDTH - (strip - 1) * H_STRIP_OFF;
       }
+      vtop.SetXYZ(xtop, y, Z);
+      vbot.SetXYZ(xbot, y, Z);
       break;
    case vertical:
+      float x;
+      auto ytop = y_start - total_height;
+      auto ybot = y_start;
       if (strip == 1) { // first strip (left)
-         vtop.SetXYZ(x_start, y_start - total_height, Z);
-         vbot.SetXYZ(x_start - EXT_STRIP_XWIDTH_L, y_start, Z);
-      } else if (strip == 184) { // last strip (right)
-         vtop.SetXYZ(x_start - EXT_STRIP_XWIDTH_L - 182 * STRIP_XWIDTH, y_start - total_height, Z);
-         vbot.SetXYZ(x_start - total_width, y_start, Z);
+         x = x_start - EXT_STRIP_XWIDTH_L / 2;
+      } else if (strip == NR_VER_STRIPS) { // last strip (right)
+         x = x_start - total_width + EXT_STRIP_XWIDTH_R / 2;
       } else { // all other vertical strips
-         vtop.SetXYZ(x_start - EXT_STRIP_XWIDTH_L - (strip - 2) * STRIP_XWIDTH, y_start - total_height, Z);
-         vbot.SetXYZ(x_start - EXT_STRIP_XWIDTH_L - (strip - 1) * STRIP_XWIDTH, y_start, Z);
+         x = x_start - EXT_STRIP_XWIDTH_L - (strip - 1.5) * STRIP_XWIDTH - (strip - 1) * V_STRIP_OFF;
       }
+      vtop.SetXYZ(x, ytop, Z);
+      vbot.SetXYZ(x, ybot, Z);
       break;
    }
 }
