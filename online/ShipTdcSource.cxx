1450a335deb4ca342fee20625a004ec5f021f400
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-09-26 16:21:22 +0200

    Reduce verbosity

--- online/ShipTdcSource.cxx
+++ online/ShipTdcSource.cxx
@@ -28,15 +28,15 @@ Int_t ShipTdcSource::UnpackEventFrame(Int_t *data, Int_t total_size)
    total_size -= sizeof(DataFrame);
    data = reinterpret_cast<Int_t *>(&(mf->hits));
    switch (mf->header.frameTime) {
-   case SoS: LOG(INFO) << "ShipTdcSource: SoS frame." << FairLogger::endl; break;
-   case EoS: LOG(INFO) << "ShipTdcSource: EoS frame." << FairLogger::endl; break;
+   case SoS: LOG(DEBUG) << "ShipTdcSource: SoS frame." << FairLogger::endl; break;
+   case EoS: LOG(DEBUG) << "ShipTdcSource: EoS frame." << FairLogger::endl; break;
    default: break;
    }
    while (total_size > 0) {
       auto df = reinterpret_cast<DataFrame *>(data);
       Int_t size = df->header.size;
       uint16_t partitionId = df->header.partitionId;
-      LOG(INFO) << "ShipTdcSource: PartitionId " << std::hex << partitionId << std::dec << FairLogger::endl;
+      LOG(DEBUG) << "ShipTdcSource: PartitionId " << std::hex << partitionId << std::dec << FairLogger::endl;
       if (!Unpack(data, size, partitionId)) {
          LOG(WARNING) << "ShipTdcSource: Failed to Unpack." << FairLogger::endl;
          LOG(WARNING) << "ShipTdcSource: Maybe missing unpacker for PartitionId " << std::hex << partitionId << std::dec
@@ -57,12 +57,12 @@ Int_t ShipTdcSource::ReadEvent(UInt_t)
       size_t size = df->header.size;
       uint16_t partitionId = df->header.partitionId;
       if (partitionId == 0x8000) {
-         LOG(INFO) << "ShipTdcSource: Event builder meta frame." << FairLogger::endl;
+         LOG(DEBUG) << "ShipTdcSource: Event builder meta frame." << FairLogger::endl;
          if (!fIn->ReadBuffer(reinterpret_cast<char *>(df->hits), size - sizeof(DataFrame))) {
             return UnpackEventFrame(reinterpret_cast<Int_t *>(&buffer), size);
          }
       }
-      LOG(INFO) << "ShipTdcSource: PartitionId " << std::hex << partitionId << std::dec << FairLogger::endl;
+      LOG(DEBUG) << "ShipTdcSource: PartitionId " << std::hex << partitionId << std::dec << FairLogger::endl;
       if (!fIn->ReadBuffer(reinterpret_cast<char *>(df->hits), size - sizeof(DataFrame))) {
 
          if (Unpack(reinterpret_cast<Int_t *>(&buffer), size, partitionId)) {
