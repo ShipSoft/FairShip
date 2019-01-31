f6f7dcc3412cdfcdf73dd763f29334431fd6595d
Nikolaus Owtscharenko <owtscharenko@physik.uni-bonn.de>
2019-01-31 10:06:13 +0100

    WIP: first try for pixel hit digitization

--- charmdet/ShipPixelHit.cxx
+++ charmdet/ShipPixelHit.cxx
@@ -1,6 +1,91 @@
 #include "ShipPixelHit.h"
+// #include "PixelDetector.h"
+#include "TVector3.h"
+#include "TGeoBBox.h"
+#include "TGeoNode.h"
+#include "TGeoManager.h"
+#include "TGeoShape.h"
+
 #include <iostream>
+#include <cmath>
 
-ShipPixelHit::ShipPixelHit(Int_t detID, Float_t digi) : ShipHit(detID, digi) {}
+// -----   Standard constructor   ------------------------------------------
+ShipPixelHit::ShipPixelHit(int32_t detID,  uint16_t tot) : ShipHit(detID, tot) {}
 
-ClassImp(ShipPixelHit)
+
+HitID ShipPixelHit::GetPixel()
+{
+  int32_t partitionID, frontEndID, row, column;
+  HitID pixelID;
+  partitionID = fDetectorID/10000000;
+  frontEndID = (fDetectorID - partitionID*10000000)/1000000;
+  row = (fDetectorID - partitionID*10000000 - frontEndID*1000000)/1000;
+  column = (fDetectorID - partitionID*10000000 - frontEndID*1000000 - row*1000);
+  // stitch together 2 Front ends to one module
+  if ((frontEndID%2)==0) column += 80;
+
+  pixelID.partitionID = partitionID;
+  pixelID.frontEndID = frontEndID;
+  pixelID.moduleID = (partitionID*frontEndID)/2;
+  pixelID.row      = row;
+  pixelID.column   = column;
+
+  return pixelID;
+}
+
+int32_t ShipPixelHit::GetModule()
+{
+  HitID pixelID;
+  pixelID = GetPixel();
+  return pixelID.moduleID;
+}
+
+void ShipPixelHit::PixelCenter(TVector3 &pixel)
+{
+  HitID hitPixelID;
+  hitPixelID = GetPixel();
+  // build node path for Geant4 geometry
+  TGeoNavigator* nav = gGeoManager->GetCurrentNavigator();
+  TString module = "volPixelModule_";
+          module += hitPixelID.moduleID;
+  TString col = "col_";
+          col += hitPixelID.column;
+  TString row = "row_";
+          row += hitPixelID.row;
+  TString path ="/";
+          path += module;
+          path +="/";
+          path +=col;
+          path +="/";
+          path +=row;
+
+  Bool_t rc = nav->cd(path);
+  if (not rc)
+  {
+    std::cout << "PixelDetector::PixelDecode, TGeoNavigator failed "<<path<<std::endl;
+    return;
+  }
+  // get pixel position and write to vector
+  TGeoNode* W = nav->GetCurrentNode();
+  TGeoBBox* S = dynamic_cast<TGeoBBox*>(W->GetVolume()->GetShape());
+  Double_t center[3] = {0,0,S->GetDZ()};
+  Double_t Gcenter[3];
+  nav->LocalToMaster(center, Gcenter);
+  pixel.SetXYZ(Gcenter[0],Gcenter[1],Gcenter[2]);
+}
+
+// -----   Destructor   ----------------------------------------------------
+// ShipPixelHit::~ShipPixelHit() = default;
+// -------------------------------------------------------------------------
+
+
+// -----   Public method Print   -------------------------------------------
+void ShipPixelHit::Print() const
+{
+  std::cout << "-I- PixelHit: Pixel hit " << " in detector " << fDetectorID << std::endl;
+  std::cout << "  ToT " << tot*25 << " ns" << std::endl; //Time over threshold in nanoseconds
+}
+// -------------------------------------------------------------------------
+
+
+ ClassImp(ShipPixelHit)
--- charmdet/ShipPixelHit.h
+++ charmdet/ShipPixelHit.h
@@ -1,19 +1,46 @@
 #ifndef PIXELHIT_H
-#define PIXELHIT_H
+#define PIXELHIT_H 1
 
 #include "ShipHit.h"
+#include "ShipOnlineDataFormat.h"
+
+struct HitID {
+   int32_t partitionID;
+   int32_t frontEndID;
+   int32_t moduleID;
+   int32_t row;
+   int32_t column;
+};
+
 
 class ShipPixelHit : public ShipHit {
 public:
-   ShipPixelHit() : ShipHit() {}
+   /** Default constructor **/
+   ShipPixelHit() = default;
    ~ShipPixelHit() = default;
-   ShipPixelHit(Int_t detID, Float_t digi);
+
+   /** Constructor with arguments
+    *@param detID    Detector ID
+    *@param tot      digitized/measured time over threshold in nanoseconds
+    *@param flags    collection of flags
+    **/
+   ShipPixelHit(int32_t detID, uint16_t tot);
+   HitID GetPixel();
+   int32_t GetDetectorID() const { return detID; }
+   int32_t GetModule();
+   void PixelCenter(TVector3 &pixel);
+   void Print() const;
+   uint16_t GetTimeOverThreshold() const { return tot*25; } // tot is measured in steps of 25 ns, so multiply by 25 to get ns value
 
 private:
-    ShipPixelHit(const ShipPixelHit& other); 
-    ShipPixelHit operator=(const ShipPixelHit& other); 
+   /** Copy constructor **/
+   ShipPixelHit(const ShipPixelHit &other);
+   ShipPixelHit operator=(const ShipPixelHit &other);
 
-   ClassDef(ShipPixelHit, 1)
+   int32_t detID;
+   uint16_t tot;
+
+   ClassDef(ShipPixelHit, 1);
 };
 
 #endif
