52cfeaa55643732918168ed8249d0317b32a0252
Matei Climescu <matei.climescu@cern.ch>
2019-02-14 12:29:47 +0100

    Fix Muflux

--- python/charmDet_conf.py
+++ python/charmDet_conf.py
@@ -42,21 +42,21 @@ def configure(run,ship_geo):
  Box.SetRunNumber(ship_geo.Box.RunNumber)
  
 
- PixelModules = ROOT.PixelModules("PixelModules",ship_geo.PixelModules.DX, ship_geo.PixelModules.DY, ship_geo.PixelModules.DZ,ROOT.kTRUE)
- PixelModules.SetSiliconDZ(ship_geo.PixelModules.DimZSi)
-
+ 
  if (ship_geo.MufluxSpectrometer.muflux==False): 
     detectorList.append(Box)
     detectorList.append(PixelModules)
-   
+    PixelModules = ROOT.PixelModules("PixelModules",ship_geo.PixelModules.DX, ship_geo.PixelModules.DY, ship_geo.PixelModules.DZ,ROOT.kTRUE)
+    PixelModules.SetSiliconDZ(ship_geo.PixelModules.DimZSi)
+    for i, (x, y, z) in enumerate(zip(ship_geo.PixelModules.xSi,ship_geo.PixelModules.ySi,ship_geo.PixelModules.zSi)): 
+    	PixelModules.SetSiliconStationPositions(i, x, y, z)
+  
  Spectrometer = ROOT.Spectrometer("Spectrometer",ship_geo.Spectrometer.DX, ship_geo.Spectrometer.DY, ship_geo.Spectrometer.DZ,ROOT.kTRUE)
  Spectrometer.SetTransverseSizes(ship_geo.Spectrometer.D1Short, ship_geo.Spectrometer.D1Long, ship_geo.Spectrometer.DSciFi1X, ship_geo.Spectrometer.DSciFi1Y, ship_geo.Spectrometer.DSciFi2X, ship_geo.Spectrometer.DSciFi2Y)   
  
 
 
- for i, (x, y, z) in enumerate(zip(ship_geo.PixelModules.xSi,ship_geo.PixelModules.ySi,ship_geo.PixelModules.zSi)): 
-  PixelModules.SetSiliconStationPositions(i, x, y, z)
-
+ 
  Spectrometer.SetSciFiDetPositions(ship_geo.Spectrometer.zSciFi1, ship_geo.Spectrometer.zSciFi2)
 # -----Goliath part by Annarita--------
  Spectrometer.SetGoliathSizes(ship_geo.Spectrometer.H, ship_geo.Spectrometer.TS, ship_geo.Spectrometer.LS, ship_geo.Spectrometer.BasisH);
