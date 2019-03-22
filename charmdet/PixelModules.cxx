1a9b6ded054e54e283335fc7df613828ef810e79
Niko <n.owtscharenko@cern.ch>
2019-03-22 23:13:48 +0100

    ENH: add alignment values, transfrom to global coordinate system

--- charmdet/PixelModules.cxx
+++ charmdet/PixelModules.cxx
@@ -153,43 +153,117 @@ std::unordered_map<int, TVector3> * PixelModules::MakePositionMap() {
 // map unique detectorID to x,y,z position in LOCAL coordinate system. xy (0,0) is on the bottom left of each Front End,
 // the raw data counts columns from 1-80 from left to right and rows from 1-336 FROM TOP TO BOTTOM.
 
-  std::unordered_map<int, TVector3> positionMap;
   const float mkm = 0.0001;
-  float  z0ref=  -1300.*mkm;
-  float  z1ref=   5200.*mkm;
-  float  z2ref=  24120.*mkm;
-  float  z3ref=  30900.*mkm;
-  float  z4ref=  51000.*mkm;
-  float  z5ref=  57900.*mkm;
-  float  z6ref=  77900.*mkm;
-  float  z7ref=  84600.*mkm;
-  float  z8ref= 104620.*mkm;
-  float  z9ref= 111700.*mkm;
-  float z10ref= 131620.*mkm;
-  float z11ref= 138500.*mkm;
 
-  float Zref[12]={z0ref, z1ref, z2ref, z3ref, z4ref, z5ref, z6ref, z7ref, z8ref, z9ref, z10ref, z11ref};
+  const float  z0ref=  -1300.*mkm;
+  const float  z1ref=   5200.*mkm;
+  const float  z2ref=  24120.*mkm;
+  const float  z3ref=  30900.*mkm;
+  const float  z4ref=  51000.*mkm;
+  const float  z5ref=  57900.*mkm;
+  const float  z6ref=  77900.*mkm;
+  const float  z7ref=  84600.*mkm;
+  const float  z8ref= 104620.*mkm;
+  const float  z9ref= 111700.*mkm;
+  const float z10ref= 131620.*mkm;
+  const float z11ref= 138500.*mkm;
 
-  int map_index=0;
+  const float Zref[12]={z0ref, z1ref, z2ref, z3ref, z4ref, z5ref, z6ref, z7ref, z8ref, z9ref, z10ref, z11ref};
+
+  const float  x0ref= 15396.*mkm      +z0ref/mkm*0.0031;
+  const float  x1ref= -2310.*mkm       +z1ref/mkm*0.0031;
+  const float  x2ref=  6960.*mkm       +z2ref/mkm*0.0031;
+  const float  x3ref=  6940.*mkm       +z3ref/mkm*0.0031;
+  const float  x4ref= 15285.*mkm       +z4ref/mkm*0.0031;
+  const float  x5ref= -2430.*mkm       +z5ref/mkm*0.0031;
+  const float  x6ref=  6620.*mkm       +z6ref/mkm*0.0031;
+  const float  x7ref=  6710.*mkm       +z7ref/mkm*0.0031;
+  const float  x8ref= 15440.*mkm       +z8ref/mkm*0.0031;
+  const float  x9ref= -2505.*mkm       +z9ref/mkm*0.0031;
+  const float x10ref=  6455.*mkm      +z10ref/mkm*0.0031;
+  const float x11ref=  6320.*mkm      +z11ref/mkm*0.0031;
+
+  const float Xref[12] { x0ref, x1ref, x2ref, x3ref, x4ref, x5ref, x6ref, x7ref, x8ref, x9ref, x10ref, x11ref};
+
+  const float  y0ref=   -15.*mkm       +z0ref*0.0068;
+  const float  y1ref=    20.*mkm       +z1ref*0.0068;
+  const float  y2ref=  7930.*mkm       +z2ref*0.0068;
+  const float  y3ref= -8990.*mkm       +z3ref*0.0068;
+  const float  y4ref=  -370.*mkm       +z4ref*0.0068;
+  const float  y5ref=  -610.*mkm       +z5ref*0.0068;
+  const float  y6ref=  7200.*mkm       +z6ref*0.0068;
+  const float  y7ref= -9285.*mkm       +z7ref*0.0068;
+  const float  y8ref=  -700.*mkm       +z8ref*0.0068;
+  const float  y9ref=  -690.*mkm       +z9ref*0.0068;
+  const float y10ref=  7660.*mkm       +z10ref*0.0068;
+  const float y11ref= -8850.*mkm      +z11ref*0.0068;
+
+  const float Yref[12] { y0ref, y1ref, y2ref, y3ref, y4ref, y5ref, y6ref, y7ref, y8ref, y9ref, y10ref, y11ref};
+
+  std::unordered_map<int, TVector3> positionMap;
+  std::vector<std::vector<TVector3>> alignment(12);
+
+  for (int i=0; i<12; i++) {
+    (*alignment[i]).SetX(Xref[i]);
+    (*alignment[i]).SetX(Yref[i]);
+    (*alignment[i]).SetZ(Zref[i]);
+  }
+
+  int map_index = 0;
   int moduleID = 0;
+  float x,x_lcoal,y, y_local;
   for (int partID=0; partID<3; partID++) {
     for (int frontEndID=0;frontEndID<8; frontEndID++ ) {
       for (int column=1; column<81; column++) {
         for (int row=1; row<337; row++) {
           map_index = 10000000*partID + 1000000*frontEndID + 1000*row + column;
           moduleID = (8*partitionID + frontEndID)/2;
-          if (frontEndID%2==0) {
-            positionMap[map_index].SetX(0.025 + (column-1)*0.025);
-            if (column == 80) positionMap[map_index].SetX(0.025 + (column-1)*0.025 + 0.0225);
+          if (frontEndID%2==1) {
+            // calculate LOCAL x position of hit
+            x_local = -0.025 - (column-1)*0.025;
+            if (column == 80) x_local -= 0.0225;
           }
-          else if (frontEndID%2==1) {
-            positionMap[map_index].SetX(0.0225 + (column-1)*0.025);
-            if (column == 80) positionMap[map_index].SetX(0.0225 + (column-1)*0.025 + 0.025);
+          else if (frontEndID%2==0) {
+            x_local = 0.0225 + (column-1)*0.025;
+            if (column == 80) x_local += 0.025;
           }
-          else {
-            printf("frontendID out of range: %i", frontEndID);
+          // calculate LOCAL y position of hit
+          y_local = 1.6775 - 0.0050*(row-1);
+          // transform local to global coordinates
+          if (frontEndID == 0){
+            x = -y_local;
+            y = x_local;
           }
-          positionMap[map_index].SetY(1.6775 - 0.0050*(row-1));
+          if (frontEndID == 1 ){
+            x = -y_local;
+            y = x_local;
+          }
+          if (frontEndID == 2 ){
+            x = y_local;
+            y = x_local;
+          }
+          if (frontEndID == 3 ) {
+            x = y_local;
+            y = x_local;
+          }
+          if (frontEndID == 4 ){
+            x = -x_local;
+            y = -y_local;
+          }
+          if (frontEndID == 5 ){
+            x = -x_local;
+            y = -y_local;
+          }
+          if (frontEndID == 6 ){
+            x = -x_local;
+            y = y_local;
+          }
+          if (frontEndID == 7 ){
+            x = -x_local;
+            y = y_local;
+          }
+          positionMap[map_index].SetX(x - Xref[moduleID]);
+          positionMap[map_index].SetY(y - Yref[moduleID]);
           positionMap[map_index].SetZ(Zref[moduleID]);
         }
       }
