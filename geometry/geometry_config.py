9445e00741651100370f8fbd8e6219bc0b063267
Thomas Ruf <Thomas.Ruf@cern.ch>
2019-04-11 15:50:36 +0200

    preparing for the fieldmap of the hadron absorber
    Muonshield design 10 will switch to using a fieldmap for the hadron absorber instead of using constants fields as with design 9 (2018 geometry). The material stays unchanged, only the field is set to 0 for the constant field in ShipMuonShield.cxx
    For testing, this can be enabled by specifying the option --muShieldDesign 10 when executing run_simScript.py

--- geometry/geometry_config.py
+++ geometry/geometry_config.py
@@ -2,7 +2,7 @@ import shipunit as u
 import ROOT as r
 from ShipGeoConfig import AttrDict, ConfigRegistry
 # the following params should be passed through 'ConfigRegistry.loadpy' method
-# muShieldDesign = 5  # 1=passive 2=active 5=TP design 6=magnetized hadron absorber
+# muShieldDesign = 5  # 1=passive 2=active 5=TP design 6=magnetized hadron absorber 9=with field map for hadron absorber
 # nuTargetPassive = 1  #0 = with active layers, 1 = only passive
 # nuTauTargetDesign  =   #0 = TP, 1 = NEW with magnet, 2 = NEW without magnet, 3 = 2018 design
 
@@ -403,6 +403,7 @@ with ConfigRegistry.register_config("basic") as c:
     if muShieldDesign > 6:  c.hadronAbsorber.length =     0*u.m # magnetized, counted inside muonshield 
     else:                   c.hadronAbsorber.length =  3.00*u.m
     c.hadronAbsorber.z     =  c.muShield.z - c.muShield.length/2. - c.hadronAbsorber.length/2.
+    if muShieldDesign > 9:  c.hadronAbsorber.WithConstField =  True
 
     c.target               =  AttrDict(z=0*u.cm)
     c.targetOpt            =  targetOpt 
