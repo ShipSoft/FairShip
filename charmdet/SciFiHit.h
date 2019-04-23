323d49c46b354fdfd94912540d574b0f08fb57a6
Ana Barbara <anabarbara.if@gmail.com>
2019-04-23 17:52:21 +0200

    Added SciFiUnpack file and modified SciFiHit class.

--- charmdet/SciFiHit.h
+++ charmdet/SciFiHit.h
@@ -7,12 +7,23 @@ class SciFiHit : public ShipHit {
 public:
    SciFiHit() : ShipHit() {}
    ~SciFiHit() = default;
-   SciFiHit(Int_t detID, Float_t digi);
+   //SciFiHit(Int_t detID, Float_t digi);
+   //SciFiHit(Int_t detID, Float_t digi, uint32_t hitTime, uint32_t fineTime, uint16_t ch, uint16_t boardId, uint16_t module, int16_t flag, bool trigFlag);
+   SciFiHit(Int_t detID, Float_t digi, uint32_t hitTime, uint32_t fineTime, uint16_t amp, uint16_t ch, uint16_t sticId, uint16_t boardId, bool flag);
    void EndPoints(TVector3 &top, TVector3 &bot);
 
 private:
     SciFiHit(const SciFiHit& other);
     SciFiHit operator=(const SciFiHit& other);
+    uint32_t hitTime;
+    uint32_t fineTime;
+    uint16_t amp;
+    uint16_t ch;
+    uint16_t sticId;
+    uint16_t boardId;
+    //uint16_t module;
+    bool flag;
+    //int16_t trigFlag;
 
    ClassDef(SciFiHit, 1)
 };
---
+++ online/SciFiUnpack.cxx
@@ -0,0 +1,129 @@
+// ROOT headers
+#include "TClonesArray.h"
+
+// SHiP headers
+#include "SciFiUnpack.h"
+#include "SciFiHit.h"
+#include "ShipOnlineDataFormat.h"
+
+// Fair headers
+#include "FairRootManager.h"
+#include "FairRunOnline.h"
+#include "FairLogger.h"
+
+struct HitData {
+   uint64_t hitword;
+};
+//SciFiUnpack::SciFiUnpack() = default; 
+// SciFiUnpack: Constructor
+//SciFiUnpack::SciFiUnpack(uint16_t PartitionId){}
+SciFiUnpack::SciFiUnpack(uint16_t PartitionId) 
+  : fRawData(new TClonesArray("SciFiHit")), fNHits(0), fNHitsTotal(0), fPartitionId(PartitionId) {}
+//SciFiUnpack::SciFiUnpack(uint16_t PartitionId) 
+//  : fRawTrigger(new TClonesArray("SciFiHit")), fNTrigs(0), fNHitsTotal(0), fPartitionId(PartitionId) {}
+
+// Virtual SciFiUnpack: Public method
+SciFiUnpack::~SciFiUnpack() = default;
+
+// Init: Public method
+Bool_t SciFiUnpack::Init()
+{ 
+   Register();
+   return kTRUE;
+}
+
+// Register: Protected method
+void SciFiUnpack::Register(){
+
+   LOG(INFO) << "SciFiUnpack : Registering..." << FairLogger::endl;
+   auto *fMan = FairRootManager::Instance();
+   if (!fMan) {
+      return;
+   }
+   fMan->Register("Digi_SciFiHits", "SciFi", fRawData, kTRUE);
+   //fMan->Register("Digi_SciFiTrigger", "SciFi", fRawTrigger, kTRUE);
+}
+
+// DoUnpack: Public method
+Bool_t SciFiUnpack::DoUnpack(Int_t *data, Int_t size)
+{
+  int nTriggers = 0;
+  int nHits = 0;
+  uint64_t aux[6];
+  aux[0]   = 0x000000000000001F;
+  aux[1]   = 0X0000000FFFFFFFFF;
+  aux[2]   = 0X00000000000000FF;
+  aux[3]   = 0X000000000000003F;
+  aux[4]   = 0X0000000000000007;
+  aux[5]   = 0X0000000000000001;
+
+
+  LOG(INFO) << "SciFiUnpack : Unpacking frame... size/bytes = " << size << FairLogger::endl;
+  auto df = reinterpret_cast<DataFrame *>(data);
+  switch (df->header.frameTime) {
+    case SoS: LOG(INFO) << "SciFiUnpack: SoS frame." << FairLogger::endl; return kTRUE;
+    case EoS: LOG(INFO) << "SciFiUnpack: EoS frame." << FairLogger::endl; return kTRUE;
+    default: break;
+  }
+
+  assert(df->header.size == size);
+  auto nhits = df->getHitCount();
+  std::vector<RawDataHit> hits(df->hits, df->hits + nhits);
+  std::cout << df->header.size << "  " << size << "  " << nhits << std::endl;
+  for (auto &&hit : hits) {
+    auto hitData = reinterpret_cast<HitData *>(&(hit.hitTime));
+    //   //auto channelId = reinterpret_cast<ChannelId *>(&(hit.channelId));
+    //   //auto detectorId = 1000000 * hitData->moduleID + 1000 * channelId->row + channelId->column;
+    auto hitinfo = hitData->hitword;
+    //uint16_t channelId       = (hitinfo >> 50);
+    //unsigned int hitTime  = (hitinfo >> 18) & 0X00000000FFFFFFFF;
+    //unsigned int fineTime = (hitinfo >> 2 ) & 0X0000000000003FFF;
+    //unsigned int flag     = (hitinfo      ) & 0X0000000000000001; 
+    unsigned int fineTime = hitinfo         & aux[0]; 
+    unsigned int hitTime  = (hitinfo >> 5)  & aux[1]; 
+    unsigned int amp      = (hitinfo >> 41) & aux[2]; 
+    unsigned int ch       = (hitinfo >> 49) & aux[3]; 
+    unsigned int sticId   = (hitinfo >> 55) & aux[4]; 
+    unsigned int boardId  = (hitinfo >> 58) & aux[3]; 
+    bool flag     = hitinfo & aux[5];
+    bool trigflag;
+
+    //std::cout << std::hex << "hitdata :)  \tfineTime:  " << fineTime 
+    //  << "\thitTime: "<< hitTime << "\tamp: " << amp << "\tch: " << ch << "\tsticId: "
+    //  << sticId << "\tboardId: " << boardId << std::endl;
+
+    //if (channelId < 15999){
+    //  trigflag = 0;
+    //  uint16_t module = channelId/1536;
+    //  if (module > 7) continue;
+    //  uint16_t ch = channelId - 1536*module;
+    //  uint16_t boardId = channelId/512;
+    //  new ((*fRawData)[fNHits]) SciFiHit(0x0900,0, hitTime*25, fineTime*32./680, ch, boardId, module, flag, trigflag);
+    //  fNHits++;
+    //}
+    //else{
+    //  trigflag = 1;
+    //  uint16_t ch = 0; //there is no channel for trigger signal
+    //  uint16_t amp = 0; //there is no amp for trigger signal
+    //  uint16_t boardId = channelId - 16000;
+    //  uint16_t module = boardId/3;
+    //  if (module > 7) continue;
+    //  new ((*fRawData)[fNHits]) SciFiHit(0x0900,0, hitTime*25, fineTime*32./680, ch, boardId, module, flag, trigflag);
+    //  fNHits++;
+    //}
+      new ((*fRawData)[fNHits]) SciFiHit(0x0900,0, hitTime*25, fineTime*32./680, amp, ch, boardId, sticId, flag);
+      fNHits++;
+  }
+
+  //fNHitsTotal += fNHits;
+  return kTRUE;
+}
+
+// Reset: Public method
+void SciFiUnpack::Reset()
+{
+  fRawData->Clear();
+  fNHits = 0;
+}
+
+ClassImp(SciFiUnpack)
