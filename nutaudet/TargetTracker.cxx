6057338efcbd839d39ca1a6bf14ea2ab1c6d03df
aleksanp <aleksandr.petrov@cern.ch>
2019-05-29 11:03:17 +0300

    Update TargetTracker.cxx

--- nutaudet/TargetTracker.cxx
+++ nutaudet/TargetTracker.cxx
@@ -93,36 +93,36 @@ fTTPointCollection(new TClonesArray("TTPoint"))
 
 TargetTracker::~TargetTracker()
 {
-  if (fTTPointCollection) {
-    fTTPointCollection->Delete();
-    delete fTTPointCollection;
-  }
+      if (fTTPointCollection) {
+        fTTPointCollection->Delete();
+        delete fTTPointCollection;
+    }
 }
 
 void TargetTracker::Initialize()
 {
-  FairDetector::Initialize();
+    FairDetector::Initialize();
 }
 
 // -----   Private method InitMedium
 Int_t TargetTracker::InitMedium(const char* name)
 {
-  static FairGeoLoader *geoLoad=FairGeoLoader::Instance();
-  static FairGeoInterface *geoFace=geoLoad->getGeoInterface();
-  static FairGeoMedia *media=geoFace->getMedia();
-  static FairGeoBuilder *geoBuild=geoLoad->getGeoBuilder();
-  
-  FairGeoMedium *ShipMedium=media->getMedium(name);
-  
-  if (!ShipMedium)
-  {
-    Fatal("InitMedium","Material %s not defined in media file.", name);
-    return -1111;
-  }
-  TGeoMedium* medium=gGeoManager->GetMedium(name);
-  if (medium!=NULL)
-    return ShipMedium->getMediumIndex();
-  return geoBuild->createMedium(ShipMedium);
+      static FairGeoLoader *geoLoad=FairGeoLoader::Instance();
+    static FairGeoInterface *geoFace=geoLoad->getGeoInterface();
+    static FairGeoMedia *media=geoFace->getMedia();
+    static FairGeoBuilder *geoBuild=geoLoad->getGeoBuilder();
+
+    FairGeoMedium *ShipMedium=media->getMedium(name);
+
+    if (!ShipMedium)
+    {
+        Fatal("InitMedium","Material %s not defined in media file.", name);
+        return -1111;
+    }
+    TGeoMedium* medium=gGeoManager->GetMedium(name);
+    if (medium!=NULL)
+        return ShipMedium->getMediumIndex();
+    return geoBuild->createMedium(ShipMedium);
 }
 
 void TargetTracker::SetSciFiParam(Double_t scifimat_width_, Double_t scifimat_hor_, Double_t scifimat_vert_, 
@@ -144,9 +144,9 @@ void TargetTracker::SetNumberSciFi(Int_t n_hor_planes_, Int_t n_vert_planes_)
 
 void TargetTracker::SetTargetTrackerParam(Double_t TTX, Double_t TTY, Double_t TTZ)
 {
-  TTrackerX = TTX;
-  TTrackerY = TTY;
-  TTrackerZ = TTZ;
+    TTrackerX = TTX;
+    TTrackerY = TTY;
+    TTrackerZ = TTZ;
 }
 
 void TargetTracker::SetBrickParam(Double_t CellW)
@@ -161,7 +161,7 @@ void TargetTracker::SetTotZDimension(Double_t Zdim)
 
 void TargetTracker::SetNumberTT(Int_t n)
 {
-  fNTT = n;
+  fNTT =n;
 }
 
 void TargetTracker::SetDesign(Int_t Design)
@@ -257,46 +257,60 @@ void TargetTracker::ConstructGeometry()
 
 Bool_t TargetTracker::ProcessHits(FairVolume* vol)
 {
-  /** This method is called from the MC stepping */
-  //Set parameters at entrance of volume. Reset ELoss.
-  if ( gMC->IsTrackEntering() ) {
-    fELoss  = 0.;
-    fTime   = gMC->TrackTime() * 1.0e09;
-    fLength = gMC->TrackLength();
-    gMC->TrackPosition(fPos);
-    gMC->TrackMomentum(fMom);
-  }
-  // Sum energy loss for all steps in the active volume
-  fELoss += gMC->Edep();
-  
-  // Create muonPoint at exit of active volume
-  if (gMC->IsTrackExiting()    ||
-      gMC->IsTrackStop()       ||
-      gMC->IsTrackDisappeared() ){
-    fTrackID  = gMC->GetStack()->GetCurrentTrackNumber();
-    gMC->CurrentVolID(fVolumeID);
-
-  	if (fELoss == 0. ) { return kFALSE; }
-    TParticle* p=gMC->GetStack()->GetCurrentTrack();
-  	Int_t fMotherID =p->GetFirstMother();
-  	Int_t pdgCode = p->GetPdgCode();
-
-    TLorentzVector Pos; 
-    gMC->TrackPosition(Pos); 
-    Double_t xmean = (fPos.X()+Pos.X())/2. ;      
-    Double_t ymean = (fPos.Y()+Pos.Y())/2. ;      
-    Double_t zmean = (fPos.Z()+Pos.Z())/2. ;     
-      
-
-    AddHit(fTrackID, fVolumeID, TVector3(xmean, ymean,  zmean),
-           TVector3(fMom.Px(), fMom.Py(), fMom.Pz()), fTime, fLength, fELoss, pdgCode);
-
-    // Increment number of muon det points in TParticle
-    ShipStack* stack = (ShipStack*) gMC->GetStack();
-    stack->AddPoint(ktauTT);
-  }
+    /** This method is called from the MC stepping */
+    //Set parameters at entrance of volume. Reset ELoss.
+    if ( gMC->IsTrackEntering() ) {
+        fELoss  = 0.;
+        fTime   = gMC->TrackTime() * 1.0e09;
+        fLength = gMC->TrackLength();
+        gMC->TrackPosition(fPos);
+        gMC->TrackMomentum(fMom);
+    }
+    // Sum energy loss for all steps in the active volume
+    fELoss += gMC->Edep();
     
-  return kTRUE;
+    // Create muonPoint at exit of active volume
+    if ( gMC->IsTrackExiting()    ||
+        gMC->IsTrackStop()       ||
+        gMC->IsTrackDisappeared()   ) {
+        fTrackID  = gMC->GetStack()->GetCurrentTrackNumber();
+        //Int_t fTrackID  = gMC->GetStack()->GetCurrentTrackNumber();
+        gMC->CurrentVolID(fVolumeID);
+	//gGeoManager->PrintOverlaps();
+	
+	//cout<< "detID = " << detID << endl;
+	Int_t MaxLevel = gGeoManager->GetLevel();
+	const Int_t MaxL = MaxLevel;
+       	//cout << gMC->CurrentVolPath()<< endl;
+	
+
+	const char *name;
+	
+	Double_t zEnd = 0, zStart =0;
+
+	
+	if (fELoss == 0. ) { return kFALSE; }
+        TParticle* p=gMC->GetStack()->GetCurrentTrack();
+	Int_t fMotherID =p->GetFirstMother();
+	Int_t pdgCode = p->GetPdgCode();
+
+        TLorentzVector Pos; 
+        gMC->TrackPosition(Pos); 
+        Double_t xmean = (fPos.X()+Pos.X())/2. ;      
+        Double_t ymean = (fPos.Y()+Pos.Y())/2. ;      
+        Double_t zmean = (fPos.Z()+Pos.Z())/2. ;     
+        
+
+	AddHit(fTrackID,fVolumeID, TVector3(xmean, ymean,  zmean),
+               TVector3(fMom.Px(), fMom.Py(), fMom.Pz()), fTime, fLength,
+               fELoss, pdgCode);
+	
+        // Increment number of muon det points in TParticle
+        ShipStack* stack = (ShipStack*) gMC->GetStack();
+        stack->AddPoint(ktauTT);
+    }
+    
+    return kTRUE;
 }
 
 
@@ -315,34 +329,38 @@ void TargetTracker::EndOfEvent()
 void TargetTracker::Register()
 {
     
-  /** This will create a branch in the output tree called
-   TargetPoint, setting the last parameter to kFALSE means:
-   this collection will not be written to the file, it will exist
-   only during the simulation.
-   */
-  
-  FairRootManager::Instance()->Register("TTPoint", "TargetTracker", fTTPointCollection, kTRUE);
+    /** This will create a branch in the output tree called
+     TargetPoint, setting the last parameter to kFALSE means:
+     this collection will not be written to the file, it will exist
+     only during the simulation.
+     */
+    
+    FairRootManager::Instance()->Register("TTPoint", "TargetTracker",
+                                          fTTPointCollection, kTRUE);
 }
 
 TClonesArray* TargetTracker::GetCollection(Int_t iColl) const
 {
-  if (iColl == 0) { return fTTPointCollection; }
-  else { return NULL; }
+    if (iColl == 0) { return fTTPointCollection; }
+    else { return NULL; }
 }
 
 void TargetTracker::Reset()
 {
-  fTTPointCollection->Clear();
+    fTTPointCollection->Clear();
 }
 
 
-TTPoint* TargetTracker::AddHit(Int_t trackID,Int_t detID, TVector3 pos, TVector3 mom,
-                               Double_t time, Double_t length, Double_t eLoss, Int_t pdgCode)
+TTPoint* TargetTracker::AddHit(Int_t trackID,Int_t detID,
+                           TVector3 pos, TVector3 mom,
+                           Double_t time, Double_t length,
+			    Double_t eLoss, Int_t pdgCode)
 {
-  TClonesArray& clref = *fTTPointCollection;
-  Int_t size = clref.GetEntriesFast();
-  //std::cout << "brick hit called"<< pos.z()<<endl;
-  return new(clref[size]) TTPoint(trackID,detID, pos, mom, time, length, eLoss, pdgCode);
+    TClonesArray& clref = *fTTPointCollection;
+    Int_t size = clref.GetEntriesFast();
+    //cout << "brick hit called"<< pos.z()<<endl;
+    return new(clref[size]) TTPoint(trackID,detID, pos, mom,
+					time, length, eLoss, pdgCode);
 }
 
 ClassImp(TargetTracker)
