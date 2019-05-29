f5000b8191ccc2392b1380473fa6b4a1b65e269a
aleksanp <aleksandr.petrov@cern.ch>
2019-05-29 10:41:30 +0300

    Update shipDet_conf.py

--- python/shipDet_conf.py
+++ python/shipDet_conf.py
@@ -406,9 +406,7 @@ def configure(run,ship_geo):
   if ship_geo.muShieldDesign==6: fMagField.IncludeTarget(ship_geo.target.xy, ship_geo.target.z0, ship_geo.target.length)
   run.SetField(fMagField)
 #
- exclusionList = ["Muon","Ecal","Hcal","Strawtubes","Veto","Magnet","MuonShield","TargetStation","TimeDet"]
- #exclusionList = ["Muon","Ecal","Hcal","Strawtubes","TargetTrackers","NuTauTarget","HighPrecisionTrackers",\
- #                 "Veto","Magnet","MuonShield","TargetStation","NuTauMudet","EmuMagnet", "TimeDet"]
+ exclusionList = []
  for x in detectorList:
    if x.GetName() in exclusionList: continue
    run.AddModule(x)
