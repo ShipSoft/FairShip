f995cd466403ad8e5db3fad6bdc17f1c7485c268
AntonioIuliano <antonio.iuliano@cern.ch>
2019-07-30 11:29:28 +0200

    updating print functions

--- macro/getGeoInformation.py
+++ macro/getGeoInformation.py
@@ -3,6 +3,7 @@
 #WARNING: printing the entire geometry takes a lot of time
 #24-02-2015 comments to EvH
 
+from __future__ import print_function, division
 import operator, sys
 from argparse import ArgumentParser
 from array import array
@@ -76,7 +77,7 @@ def print_info(path, node, level, currentlevel, print_sub_det_info=False):
       format_string += " {:10.1F}kg {:10.1F}m3"
       format_variable.extend([weight, cubicmeter])
 
-    print format_string.format(*format_variable)
+    print (format_string.format(*format_variable))
 
     if options.volume in ["", name]:
       print_sub_det_info = True
@@ -106,11 +107,11 @@ top = fGeo.GetTopVolume()
 
 
 if options.moreInfo:
- print "   Detector element             z(midpoint)     halflength       volume-start volume-end   dx"\
-       "                x-start       x-end       dy                y-start       y-end         material          weight  capacity"
+ print ("   Detector element             z(midpoint)     halflength       volume-start volume-end   dx"\
+        "                x-start       x-end       dy                y-start       y-end         material          weight  capacity")
 else:
- print "   Detector element             z(midpoint)     halflength       volume-start volume-end   dx"\
-       "                x-start       x-end       dy                y-start       y-end         material"
+ print ("   Detector element             z(midpoint)     halflength       volume-start volume-end   dx"\
+        "                x-start       x-end       dy                y-start       y-end         material")
 
 currentlevel = 0
 print_info("", top, int(options.level), currentlevel)
