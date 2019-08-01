f928cd1bfbd4ec666b7a1176c5276ece8178138d
Ana Bárbara <anabarbara.if@gmail.com>
2019-08-01 12:18:53 +0200

    Fix: x,y positions and casting issues on SciFiHit.cxx (#245)
    * Added SciFiUnpack file and modified SciFiHit class.
    
    * SciFiUnpack: included conversion of raw to root
    
    * Fix SciFi HitData struct
    
    * Modification on SciFiUnpacker: added detectorID.
    
    * Fix SciFi unpacker
    
    * Clang format
    
    * Slight refactoring
    
    * Fixed: conversion channel to detectorId. Added function  GetSciFiXYZ
    
    * Moving z to global reference system
    
    * Update SciFiUnpacker and SciFiHit
    
    * Fix conflicts merge
    
    * adding SciFi hits
    
    * Activate the SciFi on the script
    
    * Fix units on charmdet/SciFiHit.cxx
    
    * Channel mapping: Fixed.
    
    * Fix: charmdet/SciFiHit.cxx
    
    * Deleted some comments on SciFiUnpacker.cxx
    
    * Fix positions in SciFiHit.cxx
    
    * Fix: x,y positions and casting issues on SciFiHit class.
    
    * Update SciFiHit.cxx
    
    Merge together the declaration and the initialisation of ch and boardId

--- charmdet/SciFiHit.h
+++ charmdet/SciFiHit.h
@@ -9,12 +9,6 @@ public:
    ~SciFiHit() = default;
    SciFiHit(Int_t detID, Int_t layerID, uint16_t ch, uint8_t board, uint32_t hitTime, uint16_t fineTime, int flags, bool triggerFlag);
    void GetSciFiXYZ(TVector3 &v, int detID);
-   //void EndPoints(TVector3 &top, TVector3 &bot);
-   //int16_t trigFlag;
-
-
-   //SciFiHit(Int_t detID, Float_t digi);
-   //SciFiHit(Int_t detID, Float_t digi, uint32_t hitTime, uint32_t fineTime, uint16_t amp, uint16_t ch, uint16_t sticId, uint16_t boardId, bool flag);
 
 private:
     SciFiHit(const SciFiHit& other);
