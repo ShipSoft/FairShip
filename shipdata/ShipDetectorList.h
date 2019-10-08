15101fe0d8c3f1a8ca496bc5788fd9c9d036c18c
CelsoFranco-dev <56266354+CelsoFranco-dev@users.noreply.github.com>
2019-10-08 17:19:40 +0100

    Add files via upload

--- shipdata/ShipDetectorList.h
+++ shipdata/ShipDetectorList.h
@@ -8,7 +8,7 @@
 #ifndef ShipDetectorList_H
 #define ShipDetectorList_H 1
 
-// kSTOPHERE is needed for iteration over the enum. All detectors have to be put before.
+// khSTOPHERE is needed for iteration over the enum. All detectors have to be put before.
 enum DetectorId {kVETO, kTimeDet, ktauRpc, ktauHpt, ktauTT, ktauTarget, kStraw, kecal, khcal, kMuon , kPreshower, kTRSTATION, kSplitCal, kBox1, kSpectrometer, kPixelModules, kSciFi, kScintillator, kMufluxSpectrometer, kMuonTagger, kUpstreamTagger, kEndOfList};
 // last five for muonflux and Charm measurement
 #endif
