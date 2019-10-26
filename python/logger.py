f79c479a1d18c01a38708cacba593ad7dda2db7a
Jean-Loup Tastet <jean-loup.tastet@cern.ch>
2019-10-26 19:51:11 +0200

    Add a delimiter between arguments passed to FairLogger Python wrappers.
    The Python wrappers for FairLogger now take an optional `delim` keyword
    argument, which defaults to one space to match the behaviour of `print()`.
    
    The trick used to interleave delimiters between arguments is inspired
    from https://stackoverflow.com/a/7946825

--- python/logger.py
+++ python/logger.py
@@ -5,11 +5,25 @@ import ROOT
 # TODO: add header to CMake
 ROOT.gROOT.ProcessLine('#include "' + os.path.join(os.path.expandvars('$FAIRSHIP'), 'utils', 'logger.hxx') + '"')
 
-for func in ('fatal', 'error', 'warn', 'info', 'debug'):
-    # The double lambda is used in order to capture 'func' by value, not by reference
-    # (which would always point to the last value taken in the loop)
-    globals()[func] = (lambda f:
-        lambda *args: ROOT.ship.__dict__[f](*map(type, args))(*args)
-    )(func)
-    globals()[func].__doc__ = \
-        'Python wrapper around the C++ function template ship::{}(Types... args).'.format(func)
+def _make_wrapper(funcname):
+    def function(*args, **kwargs):
+        delim = kwargs.get('delim', ' ')
+        # Interleaves arguments with delimiters. No delimiter at the end.
+        args_with_delim = [val for pair in zip(args, len(args)*[delim]) for val in pair][:-1]
+        return ROOT.ship.__dict__[funcname](*map(type, args_with_delim))(*args_with_delim)
+    docstring = '''\
+    Python wrapper around the C++ function template ship::{}(Types... args).
+
+    Keyword arguments:
+    delim -- delimiter to be inserted between each argument (default \' \')
+    '''.format(funcname)
+    return function, docstring
+
+def _add_wrappers():
+    # This function is used to avoid polluting the global namespace
+    for funcname in ('fatal', 'error', 'warn', 'info', 'debug'):
+        function, docstring = _make_wrapper(funcname)
+        globals()[funcname] = function
+        globals()[funcname].__doc__ = docstring
+
+_add_wrappers()
