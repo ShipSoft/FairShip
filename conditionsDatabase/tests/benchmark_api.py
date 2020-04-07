f67067219550daa315def8558e9671573c60a800
Sadeghi <r.sadeghi@tue.nl>
2020-04-07 14:44:31 +0200

    add new tests to the benchmark_api and fix log infos and add_condition methods

--- conditionsDatabase/tests/benchmark_api.py
+++ conditionsDatabase/tests/benchmark_api.py
@@ -20,27 +20,48 @@ if __name__ == "__main__":
     cdb_api = factory.construct_DB_API("conditionsDatabase/tests/test_mongodb/test_mongodb_config.yml")
 
     # Benchmarking adding one detector to the conditions database
-    detector_and_parent = dummydata_generator.create_big_detectors(1, None, "SingleDetector", 0)
-    detector_name = detector_and_parent[0]
-    detector_parent = detector_and_parent[1]
-
     wall_time_start = time.time()
     cpu_time_start = time.clock()
 
-    cdb_api.add_detector(detector_name, detector_parent)
+    cdb_api.add_detector("detector_one")
 
     wall_time_end = time.time()
     cpu_time_end = time.clock()
     logger.info(msg='add One detector to the database%15.6f%13.6f' % (
         wall_time_end - wall_time_start, cpu_time_end - cpu_time_start))
 
+    # Benchmarking adding 100 detectors to the conditions database
+    wall_time_start = time.time()
+    cpu_time_start = time.clock()
+
+    for i in range(1, 100):
+        cdb_api.add_detector("detector_i" + i)
+
+    wall_time_end = time.time()
+    cpu_time_end = time.clock()
+    logger.info(msg='add 100 detectors to the database%15.6f%13.6f' % (
+        wall_time_end - wall_time_start, cpu_time_end - cpu_time_start))
+
+    # Benchmarking listing detectors
+    wall_time_start = time.time()
+    cpu_time_start = time.clock()
+
+    cdb_api.list_detectors()
+
+    wall_time_end = time.time()
+    cpu_time_end = time.clock()
+    logger.info(msg='List detectors%15.6f%13.6f' % (
+        wall_time_end - wall_time_start, cpu_time_end - cpu_time_start))
+
+
     # Benchmarking adding high hierarchy of detectors, subdetctors and each has
     # one condition to the condition database
     group_detector_parent = []
-    dummydata_generator.create_multilevel_detectors(5, 2, "detector", None, group_detector_parent)
+    dummydata_generator.create_multilevel_detectors(5, 5, "detector", None, group_detector_parent)
 
     wall_time_start = time.time()
     cpu_time_start = time.clock()
+
     for detector_and_parent in group_detector_parent:
         detector_name = detector_and_parent[0]
         detector_parent = detector_and_parent[1]
@@ -53,14 +74,7 @@ if __name__ == "__main__":
         else:
             reconstructed_detector_id = detector_parent + "/" + detector_name
 
-        cdb_api.add_condition(reconstructed_detector_id,
-                              "daniel",
-                              "tag1",
-                              datetime.datetime.now(),
-                              values,
-                              "calibration",
-                              datetime.datetime.now(),
-                              datetime.datetime.now())
+        cdb_api.add_condition(reconstructed_detector_id, "daniel", "tag1", values, "calibration")
 
     wall_time_end = time.time()
     cpu_time_end = time.clock()
@@ -73,13 +87,7 @@ if __name__ == "__main__":
     wall_time_start = time.time()
     cpu_time_start = time.clock()
     cdb_api.add_condition("detector1/subdetector1/subsubdetector1/subsubsubdetector1/subsubsubsubdetector1",
-                          "daniel",
-                          "tag2",
-                          datetime.datetime.now(),
-                          daniel,
-                          "calibration",
-                          datetime.datetime.now(),
-                          datetime.datetime.now())
+                          "daniel", "tag2", daniel, "calibration")
     wall_time_end = time.time()
     cpu_time_end = time.clock()
     logger.info(msg='add One massive Condition to the deepest level%15.6f%13.6f' % (
@@ -92,7 +100,7 @@ if __name__ == "__main__":
         "detector1/subdetector1/subsubdetector1/subsubsubdetector1/subsubsubsubdetector1", "daniel", "tag2")
     wall_time_end = time.time()
     cpu_time_end = time.clock()
-    logger.info(msg='add One massive Condition to the deepest level%15.6f%13.6f' % (
+    logger.info(msg='get a massive Condition from the deepest level%15.6f%13.6f' % (
         wall_time_end - wall_time_start, cpu_time_end - cpu_time_start))
 
     # Benchmarking: retrieveing a condition which is located in the deepest level of the hierarchy
@@ -102,7 +110,7 @@ if __name__ == "__main__":
         "detector1/subdetector1/subsubdetector1/subsubsubdetector1/subsubsubsubdetector1", "tag1")
     wall_time_end = time.time()
     cpu_time_end = time.clock()
-    logger.info(msg='add One massive Condition to the deepest level%15.6f%13.6f' % (
+    logger.info(msg='get Conditions by tag to the deepest level%15.6f%13.6f' % (
         wall_time_end - wall_time_start, cpu_time_end - cpu_time_start))
 
     # Benchmarking: updating a condition which is located in the deepest level of the hierarchy
