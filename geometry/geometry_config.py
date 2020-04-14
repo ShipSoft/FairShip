6cefa35ea72680fc5029cb0a3472455f6e6b33d4
dcentanni <53048804+dcentanni@users.noreply.github.com>
2020-04-14 11:52:20 +0200

    Update geometry/geometry_config.py
    Co-Authored-By: Oliver Lantwin <oliver.lantwin@cern.ch>

--- geometry/geometry_config.py
+++ geometry/geometry_config.py
@@ -631,7 +631,7 @@ with ConfigRegistry.register_config("basic") as c:
         c.tauMudet.zMudetC = c.Chamber1.z -c.chambers.Tub1length - c.tauMudet.Ztot/2 -31*u.cm;
         #lateral cuts
         c.tauMudet.CutHeight = 78.548 * u.cm
-        c.tauMudet.CutLength = (c.tauMudet.CutHeight / 2) / (r.TMath.Tan(r.TMath.DegToRad(55)))
+        c.tauMudet.CutLength = (c.tauMudet.CutHeight / 2) / (r.TMath.Tan(r.TMath.DegToRad() * 55))
         #upper cover
         c.tauMudet.XCov = c.tauMudet.XFe
         c.tauMudet.YCov = 6*u.cm
