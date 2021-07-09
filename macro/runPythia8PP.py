bd95f06bea6ebd238c8a7826c2f4bdf4efbaacaf
siilieva <sii@cern.ch>
2021-07-09 18:54:45 +0200

    Adding functionallity to have different neutrino flavours saved to output.

--- macro/runPythia8PP.py
+++ macro/runPythia8PP.py
@@ -5,14 +5,17 @@ theSeed      = 0
 ROOT.gRandom.SetSeed(theSeed)
 ROOT.gSystem.Load("libpythia8")
 
+nudict = {12: "nue", -12: "anue", 14: "numu", -14: "anumu", 16: "nutau", -16: "anutau"}
+
 from argparse import ArgumentParser
 parser = ArgumentParser()
-parser.add_argument("-b", "--heartbeat",  dest="heartbeat", type=int,  help="progress report",  default=10000)
-parser.add_argument("-n", "--pot",  dest="Np", type=int,  help="proton collisions",          default=1000000)
-parser.add_argument("-Ecm", "--energyCM",  dest="eCM", type=float,  help="center of mass energy [GeV]",          default=13000.)
-parser.add_argument('-C', '--charm', action='store_true', dest='charm',help="ccbar production",  default=False)
-parser.add_argument('-B', '--beauty', action='store_true', dest='beauty',help="bbbar production",  default=False)
-parser.add_argument('-H', '--hard', action='store_true', dest='hard',help="all hard processes",  default=False)
+parser.add_argument("-f", "--nuflavour",  action='append', type=int, dest='nu_flavour', help="neutrino flavour", default=None)
+parser.add_argument("-b", "--heartbeat",  dest="heartbeat", type=int,  help="progress report", default=10000)
+parser.add_argument("-n", "--pot",  dest="Np", type=int,  help="proton collisions", default=1000000)
+parser.add_argument("-Ecm", "--energyCM",  dest="eCM", type=float,  help="center of mass energy [GeV]", default=13000.)
+parser.add_argument('-C', '--charm', action='store_true', dest='charm',help="ccbar production", default=False)
+parser.add_argument('-B', '--beauty', action='store_true', dest='beauty',help="bbbar production", default=False)
+parser.add_argument('-H', '--hard', action='store_true', dest='hard',help="all hard processes", default=False)
 parser.add_argument('-X', '--PDFpSet',dest="PDFpSet",  type=str,  help="PDF pSet to use", default="13")
 parser.add_argument('-EtaMin',dest="eta_min",  type=float,  help="minimum eta for neutrino to pass", default=6.)
 parser.add_argument('-EtaMax',dest="eta_max",  type=float,  help="maximum eta for neutrino to pass", default=10.)
@@ -23,6 +26,12 @@ parser.add_argument('-EtaMax',dest="eta_max",  type=float,  help="maximum eta fo
 
 options = parser.parse_args()
 X=ROOT.Pythia8Generator()
+
+nunames=''
+L=len(options.nu_flavour)
+for i in range(0, L):
+  if i==L-1: nunames +=nudict[options.nu_flavour[i]]
+  else: nunames +=nudict[options.nu_flavour[i]]+'_'
 
 # Make pp events
 generator = ROOT.Pythia8.Pythia()
@@ -52,16 +61,16 @@ generator.init()
 
 rc = generator.next()
 processes = generator.info.codesHard()
-hname = 'pythia8_'+tag+'_PDFpset'+options.PDFpSet
+hname = 'pythia8_'+tag+'_PDFpset'+options.PDFpSet+'_'+nunames
 hname = hname.replace('*','star')
 hname = hname.replace('->','to')
 hname = hname.replace('/','')
 
 fout = ROOT.TFile(hname+".root","RECREATE")
-dTree = ROOT.TTree('NuTauTree','tau neutrino')
+dTree = ROOT.TTree('NuTree', nunames)
 dAnc = ROOT.TClonesArray("TParticle")
-# AncstrBranch will hold nutaus at 0th TParticle entry.
-# Nutau ancestry is followed backwards in evolution up to the colliding TeV proton
+# AncstrBranch will hold the neutrino at 0th TParticle entry.
+# Neutrino ancestry is followed backwards in evolution up to the colliding TeV proton
 # and saved as 1st, 2nd,... TParticle entries of AncstrBranch branch
 dAncstrBranch = dTree.Branch("Ancstr",dAnc,32000,-1)
 # EvtId will hold event id
@@ -76,17 +85,17 @@ py = generator
 for n in range(int(options.Np)):
   rc = py.next()
   for ii in range(1,py.event.size()):
-    # Ask for final state nutau
-    if py.event[ii].isFinal() and abs(py.event[ii].id())==16:
+    # Ask for final state neutrino
+    if py.event[ii].isFinal() and py.event[ii].id() in options.nu_flavour:
      evt = py.event[ii]
      eta = evt.eta()
      if (eta > options.eta_min and eta < options.eta_max) or (eta < -options.eta_min and eta > -options.eta_max):
        dAnc.Clear()
-       tau = ROOT.TParticle(evt.id(), evt.status(),
+       neut = ROOT.TParticle(evt.id(), evt.status(),
                             evt.mother1(),evt.mother2(),0,0,
                             evt.px(),evt.py(),evt.pz(),evt.e(),
                             evt.xProd(),evt.yProd(),evt.zProd(),evt.tProd())
-       dAnc[0] = tau
+       dAnc[0] = neut
        evtId[0] = n
        gm = py.event[ii].mother1()
        # Chain all mothers (gm)
@@ -113,11 +122,12 @@ ctime = timer.CpuTime()
 totalXsec = 0   # unit = mb,1E12 fb
 processes = generator.info.codesHard()
 for p in processes:
-   totalXsec+=generator.info.sigmaGen(p)
+   totalXsec+=generator.info.sigmaGen(p)   
 # nobias: 78.4mb, ccbar=4.47mb, bbbar=0.35mb
 
 IntLumi = options.Np / totalXsec * 1E-12
 
+print("Saving to output %s neutrino flavour(s) having PDG ID(s)"%(nunames), options.nu_flavour)
 print("simulated events = %i, equivalent to integrated luminosity of %5.2G fb-1. Real time %6.1Fs, CPU time %6.1Fs"%(options.Np,IntLumi,rtime,ctime))
 # neutrino CC cross section about 0.7 E-38 cm2 GeV-1 nucleon-1, SND@LHC: 59mm tungsten 
 # sigma_CC(100 GeV) = 4.8E-12  
