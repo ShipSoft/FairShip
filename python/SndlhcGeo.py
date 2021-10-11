3f678c4a71b0dfc5f18b58bf752c411a8e005b77
ThomasRuf <Thomas.Ruf@cern.ch>
2021-10-11 17:46:40 +0200

    module for importing geometry

---
+++ python/SndlhcGeo.py
@@ -0,0 +1,21 @@
+import ROOT
+import shipunit as u
+import shipLHC_conf as sndDet_conf
+from ShipGeoConfig import ConfigRegistry
+from rootpyPickler import Unpickler
+
+class GeoInterface():
+ " Geometry "
+ def __init__(self,geoFile):
+   self.fgeo = ROOT.TFile.Open(geoFile)
+#load geo dictionary
+   upkl    = Unpickler(self.fgeo)
+   snd_geo = upkl.load('ShipGeo')
+# -----Create geometry----------------------------------------------
+   run = "notNeeded"
+   self.modules = sndDet_conf.configure(run,snd_geo)
+   self.sGeo = self.fgeo.FAIRGeom
+   self.modules['Scifi'].SiPMmapping()
+
+ def FinishEvent(self):
+  pass
