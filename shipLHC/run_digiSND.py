0af2e892798fa6f1146d82b49e8aa7eb81410ba7
ThomasRuf <Thomas.Ruf@cern.ch>
2021-11-09 15:53:05 +0100

    adding time info for MuFilter

--- shipLHC/run_digiSND.py
+++ shipLHC/run_digiSND.py
@@ -61,7 +61,8 @@ mufiDet.SetConfPar("MuFilter/VandUpAttenuationLength",999 * u.cm)	# no significa
 mufiDet.SetConfPar("MuFilter/DsSiPMcalibrationS",25.*1000.)			# in MC: 1.65 keV are about 41.2 qdc
 mufiDet.SetConfPar("MuFilter/VandUpSiPMcalibration",25.*1000.);
 mufiDet.SetConfPar("MuFilter/VandUpSiPMcalibrationS",25.*1000.);
-
+mufiDet.SetConfPar("MuFilter/VandUpPropSpeed",12.5*u.cm/u.nanosecond);
+mufiDet.SetConfPar("MuFilter/DsPropSpeed",14.3*u.cm/u.nanosecond);
 scifiDet.SetConfPar("Scifi/nphe_min",3.5)   # threshold
 scifiDet.SetConfPar("Scifi/nphe_max ",104) # saturation
 scifiDet.SetConfPar("Scifi/timeResol",150.*u.picosecond) # time resolution in ps
