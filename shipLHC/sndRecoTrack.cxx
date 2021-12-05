f6fa8c63cecc2eee365adfcb3de96ed1336a3c7b
Cristovao Fernandes Vilela <cvilela@lxplus799.cern.ch>
2021-12-05 15:35:42 +0100

    Remove typo

--- shipLHC/sndRecoTrack.cxx
+++ shipLHC/sndRecoTrack.cxx
@@ -3,5 +3,3 @@
 sndRecoTrack::sndRecoTrack(TVector3 track_start, TVector3 track_stop, std::vector<int> hits, std::vector<int> hitsLoose) : 
   start(track_start), stop(track_stop), hits_detectorID(hits), hitsLoose_detectorID(hitsLoose) {;}
 
-typo
-
