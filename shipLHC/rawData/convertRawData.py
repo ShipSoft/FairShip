509643219d8941612007c6e51402327263d36434
ThomasRuf <Thomas.Ruf@cern.ch>
2022-01-19 15:06:33 +0100

    use board mapping from EOS

--- shipLHC/rawData/convertRawData.py
+++ shipLHC/rawData/convertRawData.py
@@ -2,6 +2,7 @@
 import ROOT,os,sys,getopt
 import shipRoot_conf
 import rootUtils as ut
+import boardMappingParser
 import numpy as np
 h={}
 
@@ -22,6 +23,7 @@ parser.add_argument("-s",dest="stop", help="do not start running", default=False
 parser.add_argument("-zM",dest="minMuHits", help="noise suppresion min MuFi hits", default=-1, type=int)
 parser.add_argument("-zS",dest="minScifiHits", help="noise suppresion min ScifFi hits", default=-1, type=int)
 parser.add_argument("-b", "--heartBeat", dest="heartBeat", help="heart beat", type=int,default=10000)
+parser.add_argument("-j", "--json", dest="json", help="read board mapping from EOS", type=int,default=1)
 
 withGeoFile = False
 
@@ -240,56 +242,18 @@ def calibrationReport():
     h['chi2'].Draw()
     return report
 
-# station mapping for SciFi
-stations = {}
-stations['M1Y'] =  {0:29, 1:3, 2:30}         # three fibre mats per plane
-stations['M1X'] =  {0:11, 1:17, 2:28}
-stations['M2Y'] =  {0:16, 1:14, 2:18}
-stations['M2X'] =  {0:1, 1:2, 2:25}
-stations['M3Y'] =  {0:15, 1:9, 2:5}
-stations['M3X'] =  {0:22, 1:27, 2:4}
-if path.find("commissioning-h6")>0: stations['M4Y'] =  {0:46, 1:40, 2:20}  # board 40 replaces 23
-else:                                                                   stations['M4Y'] =  {0:46, 1:23, 2:20}
-stations['M4X'] =  {0:8, 1:50, 2:49}
-stations['M5Y'] =  {0:19, 1:13, 2:36}
-stations['M5X'] =  {0:21, 1:10, 2:6}
+if options.json:   # read mapping from EOS
+   with client.File() as f:
+      f.open(server+path+"/board_mapping.json")
+      status, jsonStr = f.read()
 
-# board mapping for Scifi
-boardMaps = {}
-boardMaps['Scifi'] = {}
-for station in stations:
-    for mat in stations[station]:
-         board = 'board_'+str(stations[station][mat])
-         boardMaps['Scifi'][board]=[station,mat]
+  # pass the read string to getBoardMapping()
+   boardMaps = boardMappingParser.getBoardMapping(jsonStr)
 
-boardMaps['MuFilter'] = {}
-# hopefully final mapping of TI18
-boardMaps['MuFilter']['board_52'] = {'B':'Veto_1Left','C':'Veto_1Right','A':'Veto_2Left','D':'Veto_2Right'}
-boardMaps['MuFilter']['board_43'] = {'D':'US_1Left','A':'US_1Right','C':'US_2Left','B':'US_2Right'}
-boardMaps['MuFilter']['board_60'] = {'D':'US_3Left','A':'US_3Right','C':'US_4Left','B':'US_4Right'}
-boardMaps['MuFilter']['board_41'] = {'D':'US_5Left','A':'US_5Right','C':'DS_1Left','B':'DS_1Right'}
-boardMaps['MuFilter']['board_42'] = {'D':'DS_2Left','A':'DS_2Right','B':'DS_1Vert','C':'DS_2Vert'}
-boardMaps['MuFilter']['board_55'] = {'D':'DS_3Left','A':'DS_3Right','B':'DS_3Vert','C':'DS_4Vert'}
-
-if path.find("commissioning-h6")>0 or  path.find("TB_data_commissioning")>0 :
-# H6 / H8
-   boardMaps['MuFilter']['board_43'] = {'A':'US_1Left','B':'US_2Left','C':'US_2Right','D':'US_1Right'}
-   boardMaps['MuFilter']['board_60'] = {'A':'US_3Left','B':'US_4Left','C':'US_4Right','D':'US_3Right'}
-   boardMaps['MuFilter']['board_41'] = {'A':'US_5Left','B':'DS_1Left','C':'DS_1Right','D':'US_5Right'}
-   if path.find("commissioning-h6")>0: boardMaps['MuFilter']['board_59'] = {'A':'DS_2Right','B':'DS_2Vert','C':'DS_1Vert','D':'DS_2Left'}
-   else:                                                boardMaps['MuFilter']['board_59'] = {'A':'DS_2Left','B':'DS_1Vert','C':'DS_2Vert','D':'DS_2Right'}
-   boardMaps['MuFilter']['board_42'] = {'A':'DS_3Left','B':'DS_4Vert','C':'DS_3Vert','D':'DS_3Right'}
-   boardMaps['MuFilter']['board_52'] = {'A':'Veto_2Left','B':'Veto_1Left','C':'Veto_1Right','D':'Veto_2Right'}
-if path.find("data_commissioning_dune")>0:    # does not work
-   boardMaps['MuFilter']['board_43'] = {'A':'US_1Left','B':'US_2Left','C':'US_2Right','D':'US_1Right'}
-   boardMaps['MuFilter']['board_60'] = {'A':'US_3Left','B':'US_4Left','C':'US_4Right','D':'US_3Right'}
-   boardMaps['MuFilter']['board_41'] = {'A':'US_5Left','B':'DS_1Left','C':'DS_1Right','D':'US_5Right'}
-   boardMaps['MuFilter']['board_59'] = {'A':'DS_2Left','B':'DS_1Vert','C':'DS_2Vert','D':'DS_2Right'}
-   boardMaps['MuFilter']['board_42'] = {'A':'DS_3Left','B':'DS_3Vert','C':'notconnected','D':'notconnected'}
-   boardMaps['MuFilter']['board_52'] = {'A':'DS_3Right','B':'notconnected','C':'notconnected','D':'notconnected'}
+else: # old internal mapping with IF statements
+   boardMaps = boardMappingParser.oldMapping(path)
 
 slots = {0:'A',1:'A',2:'B',3:'B',4:'C',5:'C',6:'D',7:'D'}
-
 
 MufiSystem = {}
 for b in boardMaps['MuFilter']:
@@ -542,6 +506,15 @@ def run(nEvent):
 # t_fine: 0-1023, fine timestamp of the hit. It contains the raw value of the charge digitized by the TDC and requires calibration.
 # v_coarse: 0-1023, QDC mode: it represents the number of clock cycles the charge integration lasted.
 # v_fine = 0-1023, QDC mode: represents the charge measured. Requires calibration.
+
+# check against boardMappingParser
+if options.stop:
+   print('check board mapping')
+   M = boardMappingParser.oldMapping(path)
+
+   for s in M:
+      for board in M[s]:
+         print(s,board,M[s][board], boardMaps[s][board],M[s][board]==boardMaps[s][board])
 
 if not options.stop: 
    run(nEvent)
