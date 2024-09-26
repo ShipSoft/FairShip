37821314608d5b98f9573c5b9569aa65d7950889
Martina Ferrillo <martina.ferrillo@cern.ch>
2024-09-26 11:56:55 +0200

    Placeholder LLP pdgID

--- shipgen/EvtCalcGenerator.cxx
+++ shipgen/EvtCalcGenerator.cxx
@@ -101,7 +101,7 @@ Bool_t EvtCalcGenerator::ReadEvent(FairPrimaryGenerator* cpg)
   
 // Mother LLP
   Bool_t wanttracking=false;
-  pdg_llp = 9900015;
+  pdg_llp = 0.; // Geantino, placeholder
   cpg->AddTrack((Int_t)pdg_llp, px_llp, py_llp, pz_llp, vx_transf, vy_transf, vz_transf, -1., 
                 wanttracking, e_llp, tof, decay_prob);
 
