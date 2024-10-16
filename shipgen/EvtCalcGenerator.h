81063e794ebe82673ed5a41fe59e5c0711caf8ca
Oliver Lantwin <oliver.lantwin@cern.ch>
2024-10-16 13:56:18 +0200

    Autoformat EvtCalcGenerator

--- shipgen/EvtCalcGenerator.h
+++ shipgen/EvtCalcGenerator.h
@@ -1,54 +1,50 @@
-#ifndef EVTCALCGENERATOR_H
-#define EVTCALCGENERATOR_H 1
+#ifndef SHIPGEN_EVTCALCGENERATOR_H_
+#define SHIPGEN_EVTCALCGENERATOR_H_ 1
 
 #include "FairGenerator.h"
 #include "TFile.h"
 #include "TTree.h"
-#include "FairLogger.h"                 // for FairLogger, MESSAGE_ORIGIN
+
 #include <memory>
 
 class FairPrimaryGenerator;
 
 class EvtCalcGenerator : public FairGenerator
 {
- public:
+  public:
+    /** default constructor **/
+    EvtCalcGenerator();
 
-  /** default constructor **/
-  EvtCalcGenerator();
+    /** destructor **/
+    virtual ~EvtCalcGenerator();
 
-  /** destructor **/
-  virtual ~EvtCalcGenerator();
+    /** public method ReadEvent **/
+    Bool_t ReadEvent(FairPrimaryGenerator*);
+    virtual Bool_t Init(const char*, int);   //!
+    virtual Bool_t Init(const char*);        //!
 
-  /** public method ReadEvent **/
-  Bool_t ReadEvent(FairPrimaryGenerator*);
-  virtual Bool_t Init(const char*, int); //!
-  virtual Bool_t Init(const char*); //!
+    Int_t GetNevents() { return fNevents; }
 
-  Int_t GetNevents()
-  {
-    return fNevents;
-  }
+    void SetPositions(Double_t zTa, Double_t zDV)
+    {
+        ztarget = zTa;        // units cm (midpoint)
+        zDecayVolume = zDV;   // units cm (midpoint)
+    }
 
-  void SetPositions(Double_t zTa, Double_t zDV){
-    ztarget      = zTa; // units cm (midpoint)
-    zDecayVolume = zDV; // units cm (midpoint)
-  }
-
- private:
-
- protected:
-  Int_t pdg_llp, pdg_prod1, pdg_prod2, pdg_prod3;
-  Double_t px_llp, py_llp, pz_llp, e_llp;
-  Double_t vx, vy, vz, decay_prob;
-  Double_t px_prod1, py_prod1, pz_prod1, e_prod1;
-  Double_t px_prod2, py_prod2, pz_prod2, e_prod2;
-  Double_t px_prod3, py_prod3, pz_prod3, e_prod3;
-  Double_t ztarget, zDecayVolume;
-  std::unique_ptr<TFile> fInputFile; 
-  std::unique_ptr<TTree> fTree; 
-  int fNevents;
-  int fn;
-  ClassDef(EvtCalcGenerator,1);
+  private:
+  protected:
+    Int_t pdg_llp, pdg_prod1, pdg_prod2, pdg_prod3;
+    Double_t px_llp, py_llp, pz_llp, e_llp;
+    Double_t vx, vy, vz, decay_prob;
+    Double_t px_prod1, py_prod1, pz_prod1, e_prod1;
+    Double_t px_prod2, py_prod2, pz_prod2, e_prod2;
+    Double_t px_prod3, py_prod3, pz_prod3, e_prod3;
+    Double_t ztarget, zDecayVolume;
+    std::unique_ptr<TFile> fInputFile;
+    std::unique_ptr<TTree> fTree;
+    int fNevents;
+    int fn;
+    ClassDef(EvtCalcGenerator, 1);
 };
 
-#endif /* !EVTCALCGENERATOR_H */
+#endif   // SHIPGEN_EVTCALCGENERATOR_H_
