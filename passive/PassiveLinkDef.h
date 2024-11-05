312f398d5eb6eed3c0ba35b9789b16e41a7ac297
Oliver Lantwin <oliver.lantwin@cern.ch>
2024-11-05 11:15:48 +0100

    Remove last vestiges of muflux

--- passive/PassiveLinkDef.h
+++ passive/PassiveLinkDef.h
@@ -10,7 +10,6 @@
 #pragma link C++ class  ShipCave+;
 #pragma link C++ class  ShipChamber+;
 #pragma link C++ class  ShipTargetStation+;
-#pragma link C++ class  MufluxTargetStation+;
 #pragma link C++ class  ShipMuonShield+;
 #pragma link C++ class  ShipGeoCave;
 #pragma link C++ class  ShipPassiveContFact;
