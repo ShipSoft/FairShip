3c17020e9ddaa70cd8c3f445ca98aadbad087f9a
anupama-reghunath <anupamareghu@gmail.com>
2024-11-26 14:09:06 +0100

    Avoid listdir for non-directory case
    Added DIS cross section directly to the sim file
    
    Removed Condor Process variable in makeMuonDIS.py
    
    SBTchanges
    
    add default paths for MuBackground

--- shipgen/MuDISGenerator.cxx
+++ shipgen/MuDISGenerator.cxx
@@ -111,10 +111,9 @@ Double_t MuDISGenerator::MeanMaterialBudget(const Double_t* start, const Double_
   TGeoNode *currentnode = 0;
   TGeoNode *startnode = gGeoManager->InitTrack(start, dir);
   if (!startnode) {
-    //AliErrorClass(Form("start point out of geometry: x %f, y %f, z %f",
-    //		  start[0],start[1],start[2]));
-    return 0.0;
-  }
+        LOG(ERROR) << "Start point out of geometry: x " << start[0] << ", y " << start[1] << ", z " << start[2];
+        return 0.0;
+    }
   TGeoMaterial *material = startnode->GetVolume()->GetMedium()->GetMaterial();
   lparam[0]   = material->GetDensity();
   if (lparam[0] > mparam[7]) mparam[7]=lparam[0];
@@ -244,11 +243,13 @@ Bool_t MuDISGenerator::ReadEvent(FairPrimaryGenerator* cpg)
     // incoming muon  array('d',[pid,px,py,pz,E,x,y,z,w,t])
     TVectorD* mu = dynamic_cast<TVectorD*>(iMuon->AddrAt(0));
     LOG(DEBUG) << "muon DIS Generator in muon " << int(mu[0][0]);
-    Double_t x = mu[0][5] * 100.;   // come in m -> cm
-    Double_t y = mu[0][6] * 100.;   // come in m -> cm
-    Double_t z = mu[0][7] * 100.;   // come in m -> cm
-    Double_t w = mu[0][8];
-    Double_t t_muon = mu[0][11];   // in ns
+    Double_t x = mu[0][5] * 100.;                // come in m -> cm
+    Double_t y = mu[0][6] * 100.;                // come in m -> cm
+    Double_t z = mu[0][7] * 100.;                // come in m -> cm
+    Double_t w = mu[0][8];                       // weight of the original muon ( normalised to a spill)
+    Double_t cross_sec = mu[0][10];              // in mbarns
+    Double_t t_muon = mu[0][11];                 // in ns
+    Double_t DIS_multiplicity = 1 / mu[0][12];   // 1/nDIS
 
     // calculate start/end positions along this muon, and amout of material in between
 
@@ -260,9 +261,11 @@ Bool_t MuDISGenerator::ReadEvent(FairPrimaryGenerator* cpg)
     end[1] = y - (z - end[2]) * tymu;
     LOG(DEBUG) << "MuDIS: mu xyz position " << x << ", " << y << ", " << z;
     LOG(DEBUG) << "MuDIS: mu pxyz position " << mu[0][1] << ", " << mu[0][2] << ", " << mu[0][3];
-    LOG(DEBUG) << "MuDIS: mu weight " << w;
+    LOG(DEBUG) << "MuDIS: mu weight*DISmultiplicity  " << w;
+    LOG(DEBUG) << "MuDIS: mu DIS cross section " << cross_sec;
     LOG(DEBUG) << "MuDIS: start position " << start[0] << ", " << start[1] << ", " << start[2];
     LOG(DEBUG) << "MuDIS: end position " << end[0] << ", " << end[1] << ", " << end[2];
+
     Double_t bparam;
     Double_t mparam[8];
     bparam = MeanMaterialBudget(start, end, mparam);
@@ -325,12 +328,12 @@ Bool_t MuDISGenerator::ReadEvent(FairPrimaryGenerator* cpg)
                   -1,
                   false,   // tracking disabled
                   mu[0][4],
-                  t_DIS,   // shift time of the incoming muon track wrt t_muon from the input file.
-                  w);      // weight associated with a spill.
+                  t_DIS,                   // shift time of the incoming muon track wrt t_muon from the input file.
+                  w * DIS_multiplicity);   // muon weight associated with a spill* DISmultiplicity
 
     // outgoing DIS particles, [did,dpx,dpy,dpz,E], put density along trajectory as weight, g/cm^2
 
-    w = mparam[0] * mparam[4];   // modify weight, by multiplying with average densiy * track length
+    w = mparam[0] * mparam[4];   // modify weight, by multiplying with average density * track length
 
     for (auto&& particle : *dPart) {
         TVectorD* Part = dynamic_cast<TVectorD*>(particle);
@@ -339,8 +342,24 @@ Bool_t MuDISGenerator::ReadEvent(FairPrimaryGenerator* cpg)
                    << (*Part)[4];
         LOG(DEBUG) << "muon DIS Generator out part pos " << xmu << " " << ymu << "" << zmu;
         LOG(DEBUG) << "muon DIS Generator out part w " << w;
-        cpg->AddTrack(
-            int((*Part)[0]), (*Part)[1], (*Part)[2], (*Part)[3], xmu, ymu, zmu, 0, true, (*Part)[4], t_DIS, w);
+
+        if (int(mu[0][0]) == int((*Part)[0])) {
+            cpg->AddTrack(int((*Part)[0]),
+                          (*Part)[1],
+                          (*Part)[2],
+                          (*Part)[3],
+                          xmu,
+                          ymu,
+                          zmu,
+                          0,
+                          true,
+                          (*Part)[4],
+                          t_DIS,
+                          cross_sec);
+        } else {
+            cpg->AddTrack(
+                int((*Part)[0]), (*Part)[1], (*Part)[2], (*Part)[3], xmu, ymu, zmu, 0, true, (*Part)[4], t_DIS, w);
+        }
     }
 
     // Soft interaction tracks
--- shipgen/MuDISGenerator.h
+++ shipgen/MuDISGenerator.h
@@ -1,58 +1,56 @@
-#ifndef PNDMuGENERATOR_H
-#define PNDMuGENERATOR_H 1
+#ifndef SHIPGEN_MUDISGENERATOR_H_
+#define SHIPGEN_MUDISGENERATOR_H_ 1
 
-#include "TROOT.h"
 #include "FairGenerator.h"
-#include "TTree.h"                      // for TTree
-#include "TF1.h"                        // for TF1
+#include "FairLogger.h"   // for FairLogger, MESSAGE_ORIGIN
 #include "TClonesArray.h"
+#include "TF1.h"   // for TF1
+#include "TROOT.h"
+#include "TTree.h"   // for TTree
 #include "TVector3.h"
-#include "FairLogger.h"                 // for FairLogger, MESSAGE_ORIGIN
 #include "vector"
 
 class FairPrimaryGenerator;
 
 class MuDISGenerator : public FairGenerator
 {
- public:
+  public:
+    /** default constructor **/
+    MuDISGenerator();
 
-  /** default constructor **/
-  MuDISGenerator();
+    /** destructor **/
+    virtual ~MuDISGenerator();
 
-  /** destructor **/
-  virtual ~MuDISGenerator();
+    /** public method ReadEvent **/
+    Bool_t ReadEvent(FairPrimaryGenerator*);
+    virtual Bool_t Init(const char*, int);   //!
+    virtual Bool_t Init(const char*);        //!
+    Int_t GetNevents();
 
-  /** public method ReadEvent **/
-  Bool_t ReadEvent(FairPrimaryGenerator*);
-  virtual Bool_t Init(const char*, int); //!
-  virtual Bool_t Init(const char*); //!
-  Int_t GetNevents();
+    void SetPositions(Double_t z_start, Double_t z_end)
+    {
+        startZ = z_start;
+        endZ = z_end;
+    }
 
-  void SetPositions(Double_t z_start, Double_t z_end)
-  {
-      startZ = z_start;
-      endZ = z_end;
-  }
+  private:
+    Double_t MeanMaterialBudget(const Double_t* start, const Double_t* end, Double_t* mparam);
 
- private:
-  Double_t MeanMaterialBudget(const Double_t *start, const Double_t *end, Double_t *mparam);
+  protected:
+    Double_t startZ, endZ;
+    TClonesArray* iMuon;
+    TClonesArray* dPart;
+    TClonesArray* dPartSoft;
+    FairLogger* fLogger;   //!   don't make it persistent, magic ROOT command
+    TFile* fInputFile;
+    TTree* fTree;
+    int fNevents;
+    int fn;
+    bool fFirst;
 
+    const Double_t c_light = 29.9792458;              // speed of light in cm/ns
+    const Double_t muon_mass = 0.10565999895334244;   // muon mass in GeV
 
- protected:
-  Double_t startZ,endZ;
-  TClonesArray* iMuon ;
-  TClonesArray* dPart ;
-  TClonesArray* dPartSoft ;
-  FairLogger*  fLogger; //!   don't make it persistent, magic ROOT command
-  TFile* fInputFile;
-  TTree* fTree;
-  int fNevents;
-  int fn;
-  bool fFirst;
-
-  const Double_t c_light = 29.9792458;//speed of light in cm/ns
-  const Double_t muon_mass = 0.10565999895334244;  //muon mass in GeV
-  
-  ClassDef(MuDISGenerator,1);
+    ClassDef(MuDISGenerator, 1);
 };
-#endif /* !PNDMuGENERATOR_H */
+#endif   // SHIPGEN_MUDISGENERATOR_H_
