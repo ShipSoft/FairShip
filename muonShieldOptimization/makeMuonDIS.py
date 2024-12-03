5d767dcfb54e3b0651d4222fcd758d315c25fcdb
Oliver Lantwin <oliver.lantwin@cern.ch>
2024-12-03 16:18:23 +0100

    Fix #569: Use std::swap instead of TVectorD::Use()

--- muonShieldOptimization/makeMuonDIS.py
+++ muonShieldOptimization/makeMuonDIS.py
@@ -85,7 +85,9 @@ for k in range(nStart,nEnd):
   for n in range(nMult):
      dPart.Clear()
      iMuon.Clear()
-     iMuon.ConstructedAt(0).Use(muPart)
+     tca_vec = iMuon.ConstructedAt(0)
+     tca_vec.ResizeTo(muPart)
+     ROOT.std.swap(tca_vec, muPart)
      myPythia.GenerateEvent()
 # remove all unnecessary stuff
      myPythia.Pyedit(2)
@@ -99,7 +101,9 @@ for k in range(nStart,nEnd):
 # copy to branch
       nPart = dPart.GetEntries()
       if dPart.GetSize() == nPart: dPart.Expand(nPart+10)
-      dPart.ConstructedAt(nPart).Use(part)
+      tca_vec = dPart.ConstructedAt(nPart)
+      tca_vec.ResizeTo(part)
+      ROOT.std.swap(tca_vec, part)
      nMade+=1
      if nMade%10000==0: print('made so far ',nMade)
      dTree.Fill()
