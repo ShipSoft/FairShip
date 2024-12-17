3bb55f2254246b34f25d32ff4703154b055a5559
anupama-reghunath <anupamareghu@gmail.com>
2024-12-17 23:50:54 +0100

    Shift scripts to experimental / example directory

---
+++ python/experimental/__init__.py
@@ -0,0 +1,5 @@
+"""
+This is the 'experimental' package, which contains scripts that are still being tested and may undergo significant changes.
+
+Suggestions & feedback are highly appreciated as this is a work-in-progress.
+"""
