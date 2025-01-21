5eb8a643e29633777e07cab15ff8a07ca5aeff9f
anupama-reghunath <anupamareghu@gmail.com>
2025-01-21 04:00:30 +0100

    add Carbon to PDG in run_simScript

--- muonDIS/makeMuonDIS.py
+++ muonDIS/makeMuonDIS.py
@@ -12,6 +12,7 @@ from tabulate import tabulate
 logging.basicConfig(level=logging.INFO)
 PDG = r.TDatabasePDG.Instance()
 PDG.AddParticle("C12", "Carbon-12", 12.0, True, 0, 6.0, "nucleus", 1000060120)
+PDG.AddParticle("C13", "Carbon-13", 13.003355, True, 0, 6.0, "nucleus", 1000060130)
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("-f", "--inputFile", help="Input file to use", required=True)
