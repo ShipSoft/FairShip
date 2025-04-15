0a4ff5f70b7d05ef5a185bfee05b562d79bc650f
olantwin <oliver.lantwin@cern.ch>
2025-04-15 18:18:15 +0000

    Deploying to gh-pages from  @ d2b67f83187715033258e910d928a5748afa9f40 🚀

--- shipDet__conf_8py.js
+++ shipDet__conf_8py.js
@@ -1,6 +1,7 @@
 var shipDet__conf_8py =
 [
     [ "configure", "shipDet__conf_8py.html#a7f232397871353ce80c1ea03eb15bd20", null ],
+    [ "configure_snd_old", "shipDet__conf_8py.html#a6f9913f44ca120bc50d95d26f037c87c", null ],
     [ "configure_veto", "shipDet__conf_8py.html#af01a4e9be516a731b609bca5cefc2e7a", null ],
     [ "getParameter", "shipDet__conf_8py.html#a10ed9341e73de6c7f0b869a3bfbdf571", null ],
     [ "makeEcalGeoFile", "shipDet__conf_8py.html#ac8f4ac2c90998d6d936e2e21c073ca5e", null ],
