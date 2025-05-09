95f769a8be16ab637c6bf3dab21b078e1c5bb7aa
Eduard Ursov <50557695+eduard322@users.noreply.github.com>
2025-05-09 16:30:24 +0200

    Update macro/run_simScript.py
    Co-authored-by: Oliver Lantwin <oliver.lantwin@cern.ch>

--- macro/run_simScript.py
+++ macro/run_simScript.py
@@ -123,7 +123,7 @@ parser.add_argument(
 )
 
 parser.add_argument("--SND", dest="SND", help="Activate SND.", action='store_true')
-parser.add_argument("--SND_Design", dest="SND_Design", help="Choose SND design among [1,2,3,...]. 1: old version, 2: MTC", type=int, choices=[1, 2], required=False, default=1)
+parser.add_argument("--SND_Design", help="Choose SND design among [1,2,...]. 1: old version, 2: MTC", type=int, choices=[1, 2], default=1)
 parser.add_argument("--noSND", dest="SND", help="Deactivate SND. NOOP, as it currently defaults to off.", action='store_false')
 
 options = parser.parse_args()
