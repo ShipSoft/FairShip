db950e25ecc8e68ad8f563673c7a277f8f02c930
Oliver Lantwin <oliver.lantwin@cern.ch>
2025-06-12 10:48:52 +0200

    Define absolute position of spectrometer magnet

--- geometry/geometry_config.py
+++ geometry/geometry_config.py
@@ -170,12 +170,11 @@ with ConfigRegistry.register_config("basic") as c:
     # target absorber muon shield setup, decayVolume.length = nominal EOI length, only kept to define z=0
     c.decayVolume.length = 50 * u.m
 
-    c.decayVolume.z = c.muShield.z + c.decayVolume.length / 2. + c.muShield.length / 2.
-    c.decayVolume.z0 = c.decayVolume.z - c.decayVolume.length / 2.
-
     # make z coordinates for the decay volume and tracking stations relative to T4z
     # eventually, the only parameter which needs to be changed when the active shielding lenght changes.
-    c.z = 31.450 * u.m + c.decayVolume.z # Relative position of spectrometer magnet to decay vessel centre
+    c.z = 89.57 * u.m  # absolute position of spectrometer magnet
+    c.decayVolume.z = c.z - 31.450 * u.m  # Relative position of spectrometer magnet to decay vessel centre
+    c.decayVolume.z0 = c.decayVolume.z - c.decayVolume.length / 2.
     if strawDesign != 4 and strawDesign != 10:
      print("this design ",strawDesign," is not supported, use strawDesign = 4 or 10")
      1/0
