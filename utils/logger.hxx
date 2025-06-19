95c4757e6b0362df37c22a6658717fbc4352e6dc
Oliver Lantwin <oliver.lantwin@cern.ch>
2025-06-19 15:03:34 +0200

    style: enforce clang-format for SND, veto, utils

--- utils/logger.hxx
+++ utils/logger.hxx
@@ -1,11 +1,13 @@
 #pragma once
 #include "FairLogger.h"
 
-namespace ship {
+namespace ship
+{
 
-namespace details {
+namespace details
+{
 
-template <fair::Severity severity, class T>
+template<fair::Severity severity, class T>
 void log(fair::Logger& logger, T arg)
 {
     if (fair::Logger::Logging(severity)) {
@@ -13,50 +15,50 @@ void log(fair::Logger& logger, T arg)
     }
 }
 
-template <fair::Severity severity, class T, class... Types>
+template<fair::Severity severity, class T, class... Types>
 void log(fair::Logger& logger, T arg, Types... args)
 {
     log<severity>(logger, arg);
     log<severity>(logger, args...);
 }
 
-}
+}   // namespace details
 
-template <fair::Severity severity, class... Types>
+template<fair::Severity severity, class... Types>
 void log(Types... args)
 {
     fair::Logger logger(severity, __FILE__, to_string(__LINE__), __FUNCTION__);
     details::log<severity>(logger.Log(), args...);
 }
 
-template <class... Types>
+template<class... Types>
 void fatal(Types... args)
 {
     log<fair::Severity::fatal>(args...);
 }
 
-template <class... Types>
+template<class... Types>
 void error(Types... args)
 {
     log<fair::Severity::error>(args...);
 }
 
-template <class... Types>
+template<class... Types>
 void warn(Types... args)
 {
     log<fair::Severity::warning>(args...);
 }
 
-template <class... Types>
+template<class... Types>
 void info(Types... args)
 {
     log<fair::Severity::info>(args...);
 }
 
-template <class... Types>
+template<class... Types>
 void debug(Types... args)
 {
     log<fair::Severity::debug>(args...);
 }
 
-} // namespace ship
+}   // namespace ship
