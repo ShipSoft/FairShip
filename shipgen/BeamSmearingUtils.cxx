40e1f2b184719f8ba71626c3fdba9c6f8b414214
Oliver Lantwin <oliver.lantwin@cern.ch>
2025-08-07 23:05:01 +0200

    feat(shipgen): reduplicate beam smearing code

---
+++ shipgen/BeamSmearingUtils.cxx
@@ -0,0 +1,25 @@
+#include "BeamSmearingUtils.h"
+
+#include "TMath.h"
+#include "TRandom.h"
+
+std::pair<Double_t, Double_t> CalculateBeamOffset(Double_t smearBeam, Double_t paintBeam)
+{
+    Double_t dx = 0.0;
+    Double_t dy = 0.0;
+
+    // Gaussian beam smearing
+    if (smearBeam > 0) {
+        dx = gRandom->Gaus(0, smearBeam);
+        dy = gRandom->Gaus(0, smearBeam);
+    }
+
+    // Uniform circular beam painting
+    if (paintBeam > 0) {
+        Double_t phi = gRandom->Uniform(0., 2 * TMath::Pi());
+        dx += paintBeam * TMath::Cos(phi);
+        dy += paintBeam * TMath::Sin(phi);
+    }
+
+    return {dx, dy};
+}
