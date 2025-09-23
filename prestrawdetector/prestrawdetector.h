0cdd5dd8a8cf153a033ab39b3b3d9942f8488a03
AnalitikGnid <gladin.no@phystech.edu>
2025-09-23 12:14:21 +0200

    minor fixes and edits in prestrawdetector

--- prestrawdetector/prestrawdetector.h
+++ prestrawdetector/prestrawdetector.h
@@ -3,9 +3,9 @@
 
 #include "FairDetector.h"
 #include "FairVolume.h"
+#include "TVector3.h"
 #include "TClonesArray.h"
 #include "TLorentzVector.h"
-#include "TVector3.h"
 
 class prestrawdetectorPoint;
 class FairVolume;
@@ -16,62 +16,67 @@ class prestrawdetector : public FairDetector
   public:
     prestrawdetector(const char* name, Bool_t active);
     prestrawdetector();
+    prestrawdetector(std::string medium);
     virtual ~prestrawdetector();
 
     /**      Initialization of the detector is done here    */
-    virtual void Initialize();   // done
+    virtual void   Initialize(); //done
 
-    virtual Bool_t ProcessHits(FairVolume* v = 0);   // done
+    virtual Bool_t ProcessHits( FairVolume* v=0);//done
 
-    prestrawdetectorPoint* AddHit(Int_t trackID,
-                                  Int_t detID,
-                                  TVector3 pos,
-                                  TVector3 mom,
-                                  Double_t time,
-                                  Double_t length,
-                                  Double_t eLoss,
-                                  Int_t pdgCode);
-
+    prestrawdetectorPoint* AddHit(Int_t trackID, Int_t detID,
+                             TVector3 pos, TVector3 mom,
+                             Double_t time, Double_t length,
+                             Double_t eLoss, Int_t pdgCode);
+    
     /**       Registers the produced collections in FAIRRootManager.     */
-    virtual void Register();
+    virtual void   Register();
 
     /** Gets the produced collections */
-    virtual TClonesArray* GetCollection(Int_t iColl) const;   // done
+    virtual TClonesArray* GetCollection(Int_t iColl) const ; //done
 
     /**      has to be called after each event to reset the containers      */
-    virtual void Reset();
+    virtual void   Reset();
 
     void SetZ(Double_t z0);
+    void SetX(Double_t x);
+    void SetY(Double_t y);
+    void SetThickness(Double_t thickness);
 
     void ConstructGeometry();
+   
+    virtual void   CopyClones( TClonesArray* cl1,  TClonesArray* cl2 ,
+                               Int_t offset) {;}
+    virtual void   SetSpecialPhysicsCuts() {;}
+    virtual void   EndOfEvent();
+    virtual void   FinishPrimary() {;}
+    virtual void   FinishRun() {;}
+    virtual void   BeginPrimary() {;}
+    virtual void   PostTrack() {;}
+    virtual void   PreTrack() {;}
+    virtual void   BeginEvent() {;}
 
-    virtual void CopyClones(TClonesArray* cl1, TClonesArray* cl2, Int_t offset) { ; }
-    virtual void SetSpecialPhysicsCuts() { ; }
-    virtual void EndOfEvent();
-    virtual void FinishPrimary() { ; }
-    virtual void FinishRun() { ; }
-    virtual void BeginPrimary() { ; }
-    virtual void PostTrack() { ; }
-    virtual void PreTrack() { ; }
-    virtual void BeginEvent() { ; }
 
   private:
-    Double_t fPSDz;
-    std::string fMedium;
-    Int_t fTrackID;        //!  track index
-    Int_t fVolumeID;       //!  volume id
-    TLorentzVector fPos;   //!  position at entrance
-    TLorentzVector fMom;   //!  momentum at entrance
-    Double_t fTime;        //!  time
-    Double_t fLength;      //!  length
-    Double_t fELoss;
-    TClonesArray* fprestrawdetectorPointCollection;   // collection of hit points
+    Double_t     fPSDz;
+    Double_t     fPSDx;			    //! x(half of the size) in cm
+    Double_t     fPSDy;			    //! y(half of the size) in cm
+    Double_t     fPSD_Thickness;            //! thickness in cm
+    std::string fMedium; 
+    Int_t          fTrackID;                //!  track index
+    Int_t          fVolumeID;               //!  volume id
+    TLorentzVector fPos;                    //!  position at entrance
+    TLorentzVector fMom;                    //!  momentum at entrance
+    Double_t     fTime;                   //!  time
+    Double_t     fLength;                 //!  length
+    Double_t     fELoss;
+    TClonesArray* fprestrawdetectorPointCollection; // collection of hit points
 
     prestrawdetector(const prestrawdetector&);
     prestrawdetector& operator=(const prestrawdetector&);
 
     Int_t InitMedium(const char* name);
-    ClassDef(prestrawdetector, 2)
+    ClassDef(prestrawdetector,2)
 };
 
 #endif
