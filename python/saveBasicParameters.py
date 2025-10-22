ed712433499ca6ceb8089125f987ecbc2a07c2cb
Oliver Lantwin <oliver.lantwin@cern.ch>
2025-10-22 16:33:31 +0200

    fix: remove SIMPATH dependency
    SIMPATH environment variable is no longer set with AliBuild.
    Replace with EVTGENDATA for EvtGen data files, and remove
    FairSoft git tracking since it relied on SIMPATH.
    
    Closes #648

--- python/saveBasicParameters.py
+++ python/saveBasicParameters.py
@@ -7,7 +7,6 @@ def retrieveGitTags(o):
     # record some basic information about version of software:
     if "FAIRSHIP_HASH" in os.environ:
         o.FairShip = os.environ['FAIRSHIP_HASH']
-        o.FairSoft = '0000000000000000000000000000000000000000'
         o.FairRoot = os.environ['FAIRROOT_HASH']
     else:
       tmp = os.environ['FAIRSHIP']+'/.git/refs/remotes/origin/master'
@@ -18,14 +17,6 @@ def retrieveGitTags(o):
       if os.path.isfile(tmp):
         x = subprocess.check_output(['more',tmp]).replace('\n','')
         o.FairShip = AttrDict(local=x)
-        tmp = os.environ['SIMPATH']+'/../FairSoft/.git/refs/heads/master'
-      if os.path.isfile(tmp):
-        x = subprocess.check_output(['more',tmp]).replace('\n','')
-        o.FairSoft = AttrDict(master=x)
-        tmp = os.environ['SIMPATH']+'/../FairSoft/.git/refs/heads/dev'
-      if os.path.isfile(tmp):
-        x = subprocess.check_output(['more',tmp]).replace('\n','')
-        o.FairSoft = AttrDict(dev=x)
         tmp = os.environ['FAIRROOTPATH']+'/../FairRoot/.git/refs/heads/dev'
       if os.path.isfile(tmp):
         x = subprocess.check_output(['more',tmp]).replace('\n','')
