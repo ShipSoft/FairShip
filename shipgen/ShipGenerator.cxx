46b906bc85bd25a5fd4c787dcf95bd6df758b6dd
Mark Smith <mark.smith@cern.ch>
2026-02-13 20:40:31 +0100

    add copyright

--- shipgen/ShipGenerator.cxx
+++ shipgen/ShipGenerator.cxx
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-FileCopyrightText: Copyright CERN for the benefit of the SHiP
+// Collaboration
 
 #include "ShipGenerator.h"
 ShipGenerator::~ShipGenerator() {}
