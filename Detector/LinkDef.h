c18de918e43e4cb4a9de149077a31b075be97d53
Mark E Smith <mesmith@lx05.hep.ph.ic.ac.uk>
2026-03-02 14:44:39 +0000

    add DetectorPoint

---
+++ Detector/LinkDef.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-FileCopyrightText: Copyright CERN for the benefit of the SHiP Collaboration
+
+// $Id: DetectorLinkDef.h
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace SHiP;
+#pragma link C++ class SHiP::DetectorPoint+;
+
+#endif
