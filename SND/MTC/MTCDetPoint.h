58317b1325ca74fff1634175719bd9d13b5054ba
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-07-03 08:53:04 +0200

    refactor(SND): migrate trivial MC point classes to SHiP::DetectorPoint
    TTPoint, TargetPoint, MTCDetPoint and SiliconTargetPoint each re-implemented
    the 'FairMCPoint + fPdgCode' pattern that SHiP::DetectorPoint already
    provides (as vetoPoint/strawtubesPoint/etc. already use). Inherit from
    SHiP::DetectorPoint instead, keeping a thin 8-arg forwarding constructor so
    the AddHit call sites, LinkDefs and IO tests are unchanged (eventID stays 0
    as before), and preserving the detID-decoder helpers (GetLayer/GetLayerType,
    GetPlane/GetColumn/...). fPdgCode/PdgCode()/Print are now inherited.
    
    ClassDef versions are bumped; the classes gain the base's fLpos/fLmom
    (defaulted to the entrance pos/mom). Verified via the DataClassIO and
    RNtupleIO round-trip tests, which construct and read back all four classes.

--- SND/MTC/MTCDetPoint.h
+++ SND/MTC/MTCDetPoint.h
@@ -5,49 +5,38 @@
 #ifndef SND_MTC_MTCDETPOINT_H_
 #define SND_MTC_MTCDETPOINT_H_
 
-#include "FairMCPoint.h"
-#include "TObject.h"
+#include "DetectorPoint.h"
 #include "TVector3.h"
 
-class MTCDetPoint : public FairMCPoint {
+class MTCDetPoint : public SHiP::DetectorPoint {
  public:
   /** Default constructor **/
-  MTCDetPoint();
+  MTCDetPoint() = default;
 
   /** Constructor with arguments
    *@param trackID  Index of MCTrack
    *@param detID    Detector ID
-   *@param pos      Ccoordinates at entrance to active volume [cm]
+   *@param pos      Coordinates at entrance to active volume [cm]
    *@param mom      Momentum of track at entrance [GeV]
    *@param tof      Time since event start [ns]
    *@param length   Track length since creation [cm]
    *@param eLoss    Energy deposit [GeV]
+   *@param pdgCode  PDG code of the track
    **/
-
   MTCDetPoint(Int_t trackID, Int_t detID, const TVector3& pos,
               const TVector3& mom, Double_t tof, Double_t length,
-              Double_t eLoss, Int_t pdgcode);
+              Double_t eLoss, Int_t pdgCode)
+      : SHiP::DetectorPoint(0, trackID, detID, pos, mom, tof, length, eLoss,
+                            pdgCode) {}
 
-  /** Destructor **/
-  ~MTCDetPoint() override;
-
-  /** Copy constructor **/
-  MTCDetPoint(const MTCDetPoint& point) = default;
-  MTCDetPoint& operator=(const MTCDetPoint& point) = default;
-
-  /** Output to screen **/
-  using FairMCPoint::Print;
-  virtual void Print() const;
-  Int_t PdgCode() const { return fPdgCode; }
   Int_t GetLayer() const {
     return static_cast<int>(fDetectorID / 1000000) % 100;
   }
   Int_t GetLayerType() const {
     return static_cast<int>(fDetectorID / 100000) % 10;
   }
-  Int_t fPdgCode;
 
-  ClassDefOverride(MTCDetPoint, 3)
+  ClassDefOverride(MTCDetPoint, 4)
 };
 
 #endif  // SND_MTC_MTCDETPOINT_H_
