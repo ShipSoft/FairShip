e40fbed9af5c09ad1b716bbb5528400b44642e9b
eduard322 <1HArleey1@gmail.com>
2026-07-10 14:09:55 +0200

    refactor(SND): drop the SiWCalo .cxx file

--- SND/SiWCalo/SiWCaloPoint.cxx
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
-// SPDX-FileCopyrightText: Copyright CERN for the benefit of the SHiP
-// Collaboration
-
-#include "SiWCaloPoint.h"
-
-ClassImp(SiliconTargetPoint)
-
