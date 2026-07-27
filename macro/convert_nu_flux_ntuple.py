ce29dee3368b3a20ad4358cb22baf58664d6b8d0
Antonio Iuliano <antonio.iuliano@na.infn.it>
2026-07-27 14:55:09 +0200

    fix: remove EOSSHIP to preserve CERNBOX usage

--- macro/convert_nu_flux_ntuple.py
+++ macro/convert_nu_flux_ntuple.py
@@ -82,9 +82,6 @@ neutrinos = [12, 14, 16]
 cm2mm = 10.0
 nentries = 0
 
-if (inputfilename[0:4] == "/eos"):
-    inputfilename = os.environ["EOSSHIP"] + inputfilename
-
 f = ROOT.TFile.Open(inputfilename, "READ")
 print("opened file ", inputfilename)
 sTree = f.Get("cbmsim")
