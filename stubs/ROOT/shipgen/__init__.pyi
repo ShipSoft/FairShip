e6eef6ec5e88f90e78cc113192acafcab981b67f
Oliver Lantwin <oliver.lantwin@cern.ch>
2026-08-11 13:02:03 +0200

    fix(stubs): add shipgen namespace stub with MeanMaterialBudget

---
+++ stubs/ROOT/shipgen/__init__.pyi
@@ -0,0 +1,7 @@
+"""Partial type stubs for ROOT.shipgen as used in FairShip."""
+
+from __future__ import annotations
+
+from typing import Any
+
+def MeanMaterialBudget(start: Any, end: Any, mparam: Any) -> float: ...
